b45f8a009b251e7943094214417b5e61ba6ff9a8
DrugJangSu <ljonghyun6088@gmail.com>
2025-01-26 22:51:25 +0900

    Feat: 문자열 인덱싱

---
+++ multistring.py
@@ -0,0 +1,114 @@
+### 2-1. 숫자형
+
+# a=123
+# a=-178
+# a=0
+# a=0o177
+# print(a)
+# a=0x8ff
+# b=0xABC
+# print(b)
+# a=3
+# b=4
+# a+b
+# a*b
+# a/b
+# a=3
+# b=4
+# print(a**b)
+# print(7%3)
+# print(3%7)
+# print(7/4)
+# print(7//4)
+
+### 2-2. 자료형
+print("Hello World")
+print("Python is fun")
+print("""Life is too short, You need python""")
+print('''Life is too short, you need python''')
+print("Python's favorite food is perl")
+food="Python's favorite food is perl"
+print(food)
+food='Python\'s favorite food is perl'
+print(food)
+say="\"Python is very easy.\" hey says"
+print(say)
+multiline="Life is too short\nYou need python"
+print(multiline)
+head = "Python"
+tail = " is fun!"
+print(head + tail)
+a="python"
+print(a*2)
+print("="*50)
+print("My Program")
+print("="*50)
+a="Life is too short, You need python"
+print(len(a))
+a="Life is too short, You need python"
+print(a[3])
+a="Life is too short, You need python"
+print(a[0])
+print(a[12])
+print(a[-1])
+a="Life is too short, you need Python"
+b= a[0] + a[1] + a[2] + a[3]
+print(b)
+print(a[0:4])
+print(a[0:3])
+print(a[0:5])
+print(a[0:2])
+print(a[5:7])
+print(a[12:17])
+print(a[19:])
+print(a[:17])
+print(a[:])
+print(a[19:-7])
+a="20230331Rainy"
+date=a[:8]
+weather=a[8:]
+print(date)
+print(weather)
+year = a[:4]
+day = a[4:8]
+weather = a[8:]
+print(year)
+print(day)
+print(weather)
+a="Pithon"
+print(a[:1])
+print(a[2:])
+print(a[:1] + 'y' + a[2:])
+print("I eat %d apples." % 3)
+print("I eat %s apples." % "five")
+number = 3
+print("I eat %d apples." % number)
+number = 10
+day = "three"
+print("I ate %d apples. so I was sick for %s days" % (number, day))
+print("I have %s apples" % 3)
+print("rate is %s " % 3.234)
+print("%10s" % "hi")
+print("%-10sjane" % 'hi')
+print("%0.4f" % 3.42134234)
+print("%10.4f" % 3.42134234)
+print("I eat {0} apples".format(3))
+print("I eat {0} apples".format("five"))
+number = 3
+print("I eat {0} apples".format(number))
+number = 10
+day = "three"
+print("I ate {0} apples. so I was sick for {1} days".format(number, day))
+print("I ate {number} apples, so I was sick for {day} days.".format(number=10, day=3))
+
+
+
+
+
+#Python 3.13.1 (v3.13.1:06714517797, Dec  3 2024, 14:00:22) [Clang 15.0.0 (clang-1500.3.9.4)] on darwin
+#Type "help", "copyright", "credits" or "license()" for more information.
+#==================================================
+#>>> print("My Program")
+#My Program
+#>>> print("="*50)
+#==================================================
