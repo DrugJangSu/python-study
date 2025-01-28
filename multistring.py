df58fdd4bd52da1a3c6aa0b4947bd21581d22293
DrugJangSu <ljonghyun6088@gmail.com>
2025-01-28 15:59:41 +0900

    Feat: 3-3 For문

--- multistring.py
+++ multistring.py
@@ -348,169 +348,300 @@ dic={'name':'pey', 'phone':'010-9999-1234', 'birth':'1118'}
 # print(s2-s1)
 # print(s1.difference(s2))
 # print(s2.difference(s1))
-# s1=set([1,2,3])
-# s1.add(4)
-# print(s1)
-# s1=set([1,2,3])
-# s1.update([4,5,6])
-# print(s1)
-# s1=set([1,2,3])
-# s1.remove(2)
-# print(s1)
+# # s1=set([1,2,3])
+# # s1.add(4)
+# # print(s1)
+# # s1=set([1,2,3])
+# # s1.update([4,5,6])
+# # print(s1)
+# # s1=set([1,2,3])
+# # s1.remove(2)
+# # print(s1)
 
-### 2-7. 불 자료형
-# a=True
-# b=False
-# print(type(a))
-# print(type(b))
-# print(1==1)
-# print(2>1)
-# print(2<1)
-# print(2<1)
-# a=[1,2,3,4]
-# while a:
-#     print(a.pop())
-# if []:
-#     print("참")
-# else:
-#     print("거짓")
-# if [1,2,3]:
-#     print("참")
-# else:
-#     print("거짓")
-# print(bool('python'))
-# print(bool(''))
-# print(bool([1,2,3]))
-# print(bool([]))
-# print(bool(0))
-# print(bool(3))
+# ### 2-7. 불 자료형
+# # a=True
+# # b=False
+# # print(type(a))
+# # print(type(b))
+# # print(1==1)
+# # print(2>1)
+# # print(2<1)
+# # print(2<1)
+# # a=[1,2,3,4]
+# # while a:
+# #     print(a.pop())
+# # if []:
+# #     print("참")
+# # else:
+# #     print("거짓")
+# # if [1,2,3]:
+# #     print("참")
+# # else:
+# #     print("거짓")
+# # print(bool('python'))
+# # print(bool(''))
+# # print(bool([1,2,3]))
+# # print(bool([]))
+# # print(bool(0))
+# # print(bool(3))
 
-### 2-8. 자료형의 값을 저장하는 공간, 변수
-a=1
-b="python"
-c=[1,2,3]
-a=[1,2,3]
-print(id(a))
-a=[1,2,3]
-b=a
-print(id(a))
-print(id(b))
-print(a is b)
-a[1]=4
-print(a)
-print(b)
-a=[1,2,3]
-b=a[:]
-a[1]=4
-print(a)
-print(b)
-from copy import copy
-a=[1,2,3]
-b=copy(a)
-print(b is a)
+# ### 2-8. 자료형의 값을 저장하는 공간, 변수
+# a=1
+# b="python"
+# c=[1,2,3]
+# a=[1,2,3]
+# print(id(a))
+# a=[1,2,3]
+# b=a
+# print(id(a))
+# print(id(b))
+# print(a is b)
+# a[1]=4
+# print(a)
+# print(b)
+# a=[1,2,3]
+# b=a[:]
+# a[1]=4
+# print(a)
+# print(b)
+# from copy import copy
+# a=[1,2,3]
+# b=copy(a)
+# print(b is a)
 
-a,b=('python', 'life')
-(a,b)='python', 'Life'
-[a,b]='python', 'Life'
-a=b='python'
-a=3
-b=5
-a,b=b,a
-print(a)
-print(b)
+# a,b=('python', 'life')
+# (a,b)='python', 'Life'
+# [a,b]='python', 'Life'
+# a=b='python'
+# a=3
+# b=5
+# a,b=b,a
+# print(a)
+# print(b)
 
 
-### 3-1. If문
-money = True
-if money:
-    print("택시를 타고 가라")
-else:
-    print("걸어가라")
-# money=True
+# ### 3-1. If문
+# money = True
 # if money:
 #     print("택시를 타고 가라")
-# print("타고")
-#     print("가라")
-# money=True
-# if money:
-#     print("택시를")
-#     print("타고")
-#        print("가라")
+# else:
+#     print("걸어가라")
+# # money=True
+# # if money:
+# #     print("택시를 타고 가라")
+# # print("타고")
+# #     print("가라")
+# # money=True
+# # if money:
+# #     print("택시를")
+# #     print("타고")
+# #        print("가라")
     
-# money=True
-# if money:
+# # money=True
+# # if money:
 
-x=3
-y=2
-print(x>y)
-print(x<y)
-print(x==y)
-print(x!=y)
-money=2000
-if money >= 3000:
-    print("택시를 타고 가라")
-else:
-    print("걸어가라")
-money=2000
-card=True
-if money >= 3000 or card:
-    print("택시를 타고 가라")
-else:
-    print("걸어가라")
-print(1 in [1,2,3])
-print(1 not in [1,2,3])
-print('a' in ('a','b','c'))
-print('j' not in 'python')
-pocket=['paper', 'cellphone', 'money']
-if 'money' in pocket:
-    print("택시를 타고 가라")
-else:
-    print("걸어가라")
+# x=3
+# y=2
+# print(x>y)
+# print(x<y)
+# print(x==y)
+# print(x!=y)
+# money=2000
+# if money >= 3000:
+#     print("택시를 타고 가라")
+# else:
+#     print("걸어가라")
+# money=2000
+# card=True
+# if money >= 3000 or card:
+#     print("택시를 타고 가라")
+# else:
+#     print("걸어가라")
+# print(1 in [1,2,3])
+# print(1 not in [1,2,3])
+# print('a' in ('a','b','c'))
+# print('j' not in 'python')
+# pocket=['paper', 'cellphone', 'money']
+# if 'money' in pocket:
+#     print("택시를 타고 가라")
+# else:
+#     print("걸어가라")
 
-pocket=['paper', 'cellphone', 'money']
-if 'card' not in pocket:
-    print("걸어가라")
-else:
-    print("버스를 타고 가라")
+# pocket=['paper', 'cellphone', 'money']
+# if 'card' not in pocket:
+#     print("걸어가라")
+# else:
+#     print("버스를 타고 가라")
 
-pocket=['paper', 'money', 'cellphone']
-if 'money' in pocket:
-    pass
-else:
-    print("카드를 꺼내라")
-print("앙기모찌")
-pocket = ['paper', 'handphone']
-card = True
-if 'money' in pocket:
-    print("택시를 타고 가라")
-else:
-    if card:
-        print("택시를 타고 가라")
-    else:
-        print("걸어가라")
-pocket=['paper', 'cellphone']
-card=True
-if 'money' in pocket:
-    print("택시를 타고 가라")
-elif card:
-    print("택시를 타고 가라")
-else:
-    print("걸어가라")
-if 'money' in pocket:
-    pass
-else:
-    print("카드를 꺼내라")
-pocket=['paper', 'money', 'cellphone']
-if 'money' in pocket: pass
-else: print("카드를 꺼내라")
+# pocket=['paper', 'money', 'cellphone']
+# if 'money' in pocket:
+#     pass
+# else:
+#     print("카드를 꺼내라")
+# print("앙기모찌")
+# pocket = ['paper', 'handphone']
+# card = True
+# if 'money' in pocket:
+#     print("택시를 타고 가라")
+# else:
+#     if card:
+#         print("택시를 타고 가라")
+#     else:
+#         print("걸어가라")
+# pocket=['paper', 'cellphone']
+# card=True
+# if 'money' in pocket:
+#     print("택시를 타고 가라")
+# elif card:
+#     print("택시를 타고 가라")
+# else:
+#     print("걸어가라")
+# if 'money' in pocket:
+#     pass
+# else:
+#     print("카드를 꺼내라")
+# pocket=['paper', 'money', 'cellphone']
+# if 'money' in pocket: pass
+# else: print("카드를 꺼내라")
 
-if score >= 60:
-    message = "success"
-else:
-    message = "failure"
-message = "success" if score >= 60 else "failure"
+# if score >= 60:
+#     message = "success"
+# else:
+#     message = "failure"
+# message = "success" if score >= 60 else "failure"
 
-### 3-2 while 문
+# ### 3-2 while 문
+# treeHit=0
+# while treeHit <10:
+#     treeHit=treeHit+1
+#     print("나무를 %d번 찍었습니다." % treeHit)
+#     if treeHit==10:
+#         print("나무 넘어갑니다.")
+
+# prompt = """
+# 1. Add
+# 2. Del
+# 3. List
+# 4. Quit
+# Enter number: """
+# number=0
+# while number != 4:
+#     print(prompt)
+#     number=int(input())
+
+# coffee = 10
+# money=300
+# while money:
+#     print("돈을 받았으니 커피를 줍니다")
+#     coffee=coffee-1
+#     print("남은 커피의 양은 %d개입니다." % coffee)
+#     if coffee==0:
+#         print("커피가 다 떨어졌습니다. 판매를 중지합니다.")
+#         break
+# coffee=10
+# while True:
+#     money=int(input("돈을 넣어주세요: "))
+#     if money==300:
+#         print("커피를 줍니다.")
+#         coffee = coffee - 1
+#     elif money>300:
+#         print("거스름돈 %d를 주고 커피를 줍니다." % (money-300))
+#         coffee = coffee - 1
+#     else:
+#         print("돈을 다시 돌려주고 커피를 주지 않습니다.")
+#         print("남은 커피의 양은 %d개입니다." % coffee)
+#     if coffee==0:
+#         print("커피가 다 떨어졌습니다. 판매를 중지합니다.")
+#         break
+
+# a=0
+# while a<10:
+#     a=a+1
+#     if a%2==0: continue
+#     print(a)
+
+# a=0
+# while a<10:
+#     a=a+1
+#     if a%3==0: continue
+#     print(a)
+
+# a=0
+# while a<10:
+#     a=a+1
+#     if a%3|0: continue
+#     print(a)
+
+# ### 3-3 for 문
+
+# test_list = ['one', 'two', 'three']
+# for i in test_list:
+#     print(i)
+
+# a=[(1,2), (3,4), (5,6)]
+# for (first, last) in a:
+#     print(first+last)
+
+# marks = [90, 25, 67, 45, 80]
+# number=0
+# for mark in marks:
+#     number=number+1
+#     if mark>=60:
+#         print("%d번 학생은 합격입니다." % number)
+#     else:
+#         print("%d번 학생은 불합격입니다." % number)
+
+# marks=[90, 25, 67, 45, 80]
+# number=0
+# for mark in marks:
+#     number=number+1
+#     if mark<60: continue
+#     print("%d번 학생 축하합니다. 합격입니다." % number)
+
+# a=range(10)
+# print(a)
+
+# a=range(1,11)
+# print(a)
+
+# add=0
+# for i in range(1,11):
+#     add=add+i
+# print(add)
+
+# marks=[90, 25, 67, 45, 80]
+# for number in range(len(marks)):
+#     if marks[number]<60: continue
+#     print("%d번 학생 축하합니다. 합격입니다." % (number+1))
+
+# add=0
+# for i in range(1,101):
+#     add=add+i
+# print(add)
+
+# for i in range(2, 10):
+#     for j in range(1, 10):
+#         print(i*j, end=" ")
+#     print('')
+
+# a=[1, 2, 3, 4]
+# result=[]
+# for num in a:
+#     result.append(num*3)
+# print(result)
+
+a=[1, 2, 3, 4]
+result=[num*3 for num in a]
+print(result)
+
+a=[1, 2, 3, 4]
+result=[num*3 for num in a if num%2==0]
+print(result)
+
+result=[x*y for x in range(2,10)
+            for y in range(1,10)]
+print(result)
+
 
 
 #Python 3.13.1 (v3.13.1:06714517797, Dec  3 2024, 14:00:22) [Clang 15.0.0 (clang-1500.3.9.4)] on darwin
