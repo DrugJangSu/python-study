b37b93ac324bbf4d57fb1d6e793c39b8967ea2b3
DrugJangSu <ljonghyun6088@gmail.com>
2025-06-19 19:49:18 +0900

    Update

--- 100_days_of_python.py
+++ 100_days_of_python.py
@@ -4,4 +4,39 @@
 # print("Hello world!")
 # print("Hello world!\nHello world!\nHello world!")
 # print("Hello" + "Angela")
-print("Hello" + " " + "Angela")
+# print("Hello" + " " + "Angela")
+# input("What is your name?")
+# print("Hello " + input("What is your name?"))
+# print("Hello " + input("What is your name?") + "!")
+# You can write it as much as you want, but it won't be excuted as long as you put the '#' in front of the line.
+# This line of code will take in input using the input() function.
+# name = input("What is your name?")
+# print(name)
+# name = "Jack"
+# print(name)
+# name= "Angela"
+# print(name)
+# print(len(name))
+# print(len(input("What is your name?")))
+# username =input("What is your name?")
+# length =len(username)
+# print(length)
+# n= "Angela"
+# l = len(n)
+# print(l)
+# user_name_of_this_person = "Angela"
+# l = len(user_name_of_this_person)
+# print(l)
+# nama = "Angela"
+# length = len(nama)
+# print(length)
+# print("Welcome to the Band Name Generator.")
+# city = input("What''s the name of the city you grew up in?\n")
+# print(city)
+# pet = input("What's your pet's name?\n")
+# print(pet)
+# print("Your band name could be: " + city + " " +  pet)
+print("Welcome to the Band Name Generator.")
+City = input("What's the name of the city you grew up in?\n")
+Pet = input("What's your pet's name?\n")
+print("Your band name could be: " + City + " " + Pet)
