522df1cce5384257b3b192a300cb208192288c7f
DrugJangSu <ljonghyun6088@gmail.com>
2025-06-19 23:30:37 +0900

    Update

--- 100_days_of_python.py
+++ 100_days_of_python.py
@@ -150,6 +150,7 @@
 # print(f"Each person should pay: ${final_amount}")
 
 # # Day 3 : Beginner - Control Flow and Logical Operators
+
 # print("Welcome to the rollercoaster!")
 # height = int(input("What is your height in cm? "))
 # # if height >= 120:
@@ -164,7 +165,31 @@
 
 # if height != 120:
 #     print("You can ride the rollercoaster!")
-# else:
-#     print("Sorry, you have to grow taller before you can ride.")
+# # else:
+# #     print("Sorry, you have to grow taller before you can ride.")
 
+
+# number_to_check = int(input("What is the number you want to check? \n"))
+# # print(number_to_check % 2) 
+# if number_to_check % 2 == 0:
+#     print("This is an even number.")
+# else:
+#     print("This is an odd number.")
+
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+
+if height >= 120:
+    print("You can ride the rollercoaster!")
+    age = int(input("What is your age? "))
+    if age <= 12:
+        print("Please pay $5.")
+    elif age <= 18:
+        print("Please pay $7.")
+    elif age <= 22:
+        print("Please pay $10.")
+    else:
+        print("Please pay $12.")
+else:
+    print("Sorry, you have to grow taller before you can ride.")
 
