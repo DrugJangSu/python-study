51f4b6e635c8230376246adfb93a7dd4d9fb45f8
DrugJangSu <ljonghyun6088@gmail.com>
2025-06-19 18:23:07 +0900

    Update Day 1 code to print Hello Angela

--- 100_days_of_python.py
+++ 100_days_of_python.py
@@ -1,2 +1,7 @@
 # Day 1 : Beginner - Working with Variables in Python to Manage Data
-print("Hello world!")
+# print("Hello world!")
+# print("Hello world!")
+# print("Hello world!")
+# print("Hello world!\nHello world!\nHello world!")
+# print("Hello" + "Angela")
+print("Hello" + " " + "Angela")
