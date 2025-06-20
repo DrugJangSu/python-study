1ded027504dc557282fb96f3d245e70689f1234c
DrugJangSu <ljonghyun6088@gmail.com>
2025-06-20 19:53:47 +0900

    Update

--- 100_days_of_python.py
+++ 100_days_of_python.py
@@ -291,3 +291,91 @@
 # print(f"Your final bill is ${bill}")
 
 
+# print("Welcome to the rollercoaster!")
+# height = int(input("What is your height in cm? "))
+# bill = 0
+
+# if height >= 120:
+#     print("You can ride the rollercoaster!")
+#     age = int(input("What is your age? "))
+#     if age <= 12:
+#         bill = 5
+#         print("Child tickets are $5.")
+#     elif age <= 18:
+#         bill = 7
+#         print("Youth tickets are $7.")
+#     elif age >= 45 and age <= 55: #or you can write it as 45 <= age <= 55
+#         print("Everything is going to be ok. Have a free ride on us!")
+#         # No bill for this age group -> we don't need to modify the bill variable because we know that once this condition is met, the bill will remain 0. And it will skip this whole if block and move on.
+#     else:
+#         bill = 12
+#         print("Adult tickets are $12.")
+
+#     wants_photo = input("Do you want a photo taken? Y for Yes, N for No: ")
+#     if wants_photo == "Y":
+#         # Add $3 to their bill for the photo
+#         bill += 3
+    
+#     print(f"Your final bill is: ${bill}")
+# else:
+#     print("Sorry, you have to grow taller before you can ride.")
+
+
+# Day 3 final project(Treasure Island)
+
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/______/
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.")
+choice1 = input('You\'re at a crossroad. Where do you want to go? ' \
+'Type "left" or "right".\n').lower()
+ # Using .lower() to make the input case-insensitive.
+ # when you put \ before the ' it will not be treated as a string and will be treated as a character.
+
+if choice1 == "left":
+    choice2 = input('You\'ve come to a lake. There is an island in the middle of the lake. ' \
+    'Type "wait" to wait for a boat. Type "swim" to swim across.\n').lower()
+    if choice2 == "wait":
+        choice3 = input('You arrive at the island unharmed. There is house with 3 doors. ' \
+                        'One "red", one "yellow", and one "blue". Which color do you choose?\n').lower()
+        if choice3 == "red":
+            print("It's a room full of fire. Game Over.")
+        elif choice3 == "yellow":
+            print("You found the treasure! You Win!")
+        elif choice3 == "blue":
+            print("You enter a room of beasts. Game Over.")
+        else:
+            print("You chose a door that doesn't exist. Game Over.")
+
+    else :
+        print("You got attacked by a trout. Game Over.")
+
+# <Format 1>
+else :
+    print("You fell into a hole. Game Over.")
+
+#<Format 2>
+# if choice1 == "right":
+#     print("You fell into a hole. Game Over.")
+
+#If you want to countinue the game, you'll use the first format rather than the second one.
