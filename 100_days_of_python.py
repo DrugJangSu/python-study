22304c7e2f0b4a54ec6b9a6c7f9e589e8c303497
DrugJangSu <ljonghyun6088@gmail.com>
2025-06-20 14:51:11 +0900

    Update

--- 100_days_of_python.py
+++ 100_days_of_python.py
@@ -176,20 +176,118 @@
 # else:
 #     print("This is an odd number.")
 
-print("Welcome to the rollercoaster!")
-height = int(input("What is your height in cm? "))
+# print("Welcome to the rollercoaster!")
+# height = int(input("What is your height in cm? "))
 
-if height >= 120:
-    print("You can ride the rollercoaster!")
-    age = int(input("What is your age? "))
-    if age <= 12:
-        print("Please pay $5.")
-    elif age <= 18:
-        print("Please pay $7.")
-    elif age <= 22:
-        print("Please pay $10.")
-    else:
-        print("Please pay $12.")
-else:
-    print("Sorry, you have to grow taller before you can ride.")
+# if height >= 120:
+#     print("You can ride the rollercoaster!")
+#     age = int(input("What is your age? "))
+#     if age <= 12:
+#         print("Please pay $5.")
+#     elif age <= 18:
+#         print("Please pay $7.")
+#     elif age <= 22:
+#         print("Please pay $10.")
+#     else:
+#         print("Please pay $12.")
+# else:
+#     print("Sorry, you have to grow taller before you can ride.")
+
+
+# print("Welcome to the rollercoaster!")
+# height = int(input("What is your height in cm? "))
+# bill = 0
+
+# if height >= 120:
+#     print("You can ride the rollercoaster!")
+#     age = int(input("What is your age? "))
+#     if age <= 12:
+#         bill = 5
+#         print("Child tickets are $5.")
+#     elif age <= 18:
+#         bill = 7
+#         print("Youth tickets are $7.")
+#     else:
+#         bill = 12
+#         print("Adult tickets are $12.")
+
+#     wants_photo = input("Do you want a photo taken? Y for Yes, N for No: ")
+#     if wants_photo == "Y":
+#         # Add $3 to their bill for the photo
+#         bill += 3
+    
+#     print(f"Your final bill is: ${bill}")
+# else:
+#     print("Sorry, you have to grow taller before you can ride.")
+
+
+# Pizza Order Exercise
+# print("Welcome to Python Pizza Deliveries!")
+# size = input("What size pizza do you want? S, M, or L: ")
+# pepperoni = input("Do you want pepperoni? Y or N: ")
+# # extra cheese = input("Do you want extra cheese? Y or N: ")
+
+# todo : work out how much they need to pay based on their size choice.
+# todo : work out how much to add to their bill based on their pepperoni choice.
+# todo : work out how much to add to their bill if they want extra cheese.
+
+# <My own solution>--------------------------------------
+# print("Welcome to Python Pizza Deliveries!")
+# size = input("What size pizza do you want? S, M, or L: ")
+# bill = 0
+
+# if size == "S":
+#     bill = 15
+#     print("Small pizza costs $15.")
+# elif size == "M":
+#     bill = 20
+#     print("Medium pizza costs $20.")
+# elif size == "L":
+#     bill = 25
+#     print("Large pizza costs $25.")
+
+# wants_pepperoni = input("Do you want pepperoni? Y or N: ")
+# if wants_pepperoni == "Y":
+#     if size == "S":
+#         bill += 2
+#         print("Small pizza with pepperoni costs $2 extra.")
+#     else:
+#         bill += 3
+#         print("Medium or Large pizza with pepperoni costs $3 extra.")
+# wants_extra_cheese = input("Do you want extra cheese? Y or N: ")
+# if wants_extra_cheese == "Y":
+#     bill += 1
+#     print("Extra cheese costs $1 extra.")
+# print(f"Your final bill is: ${bill}")
+# --------------------------------------------------
+# <Instructor's solution>
+
+# print("Welcome to Python Pizza Deliveries!")
+# size = input("What size pizza do you want? S, M, or L: ")
+# pepperoni = input("Do you want pepperoni? Y or N: ")
+# extra_cheese = input("Do you want extra cheese? Y or N: ")
+
+# bill = 0
+# if size =="S":
+#     bill += 15
+# elif size == "M":
+#     bill += 20
+# elif size == "L":
+#     bill += 25
+# else:
+#     print("You typed the wrong inputs.")
+#  # else option also works here because there are only 3 options for size.
+
+# # todo : work out how much to add to their bill based on their pepperoni choice.
+# if pepperoni == "Y":
+#     if size == "S":
+#         bill += 2
+#     else:
+#         bill += 3
+
+# # todo : work out how much to add to their bill if they want extra cheese.
+# if extra_cheese == "Y":
+#     bill += 1
+# print(f"Your final bill is ${bill}")
+
 
