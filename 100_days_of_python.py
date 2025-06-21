707c239bf62f515d80052fa925f87ea0d8b8a0d6
DrugJangSu <ljonghyun6088@gmail.com>
2025-06-21 20:23:06 +0900

    Update

--- 100_days_of_python.py
+++ 100_days_of_python.py
@@ -536,9 +536,120 @@
 
 # ----end of Day 4 Project---------------
 
-# Day 5 : Beginner - Python Loops----------------------------------------
+# Day 5 : Beginner - Python Loops---------------------------------------------------
 
-fruits = ["Apple", "Peach", "Pear"]
-for fruit in fruits:
-    print(fruit)
-    
+# fruits = ["Apple", "Peach", "Pear"]
+# for fruit in fruits:
+#     print(fruit)
+#     print(fruit + " pie")
+#     print(fruits)
+# print(fruits)
+
+
+# student_scores = [150, 142, 185, 120, 171, 184, 149, 24, 59, 68, 199, 78, 65, 89, 86, 55, 91, 64, 89]
+
+# total_exam_score = sum(student_scores)
+# sum = 0
+# for score in student_scores:
+#     sum += score
+# print(sum)
+
+
+# student_scores = [150, 142, 185, 120, 171, 184, 149, 24, 59, 68, 199, 78, 65, 89, 86, 55, 91, 64, 89]
+
+
+# max_score = 0
+
+# for score in student_scores:
+#     if score > max_score:
+#         max_score = score
+
+# print(max_score)
+
+## It's really important to understand the for - in loop function.
+
+# Range function
+
+# for number in range(1, 11, 3):
+#     print(number)
+# "3" means 1 + 3 + 3 + 3...
+
+# total = 0
+# for number in range(1, 101):
+#     total += number
+# print(total)
+
+# --------day 5 project : create a Password Generator----------------
+
+# import random
+# letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g','h', 'i', 'j', 'k', 'l', 'm', 'n','o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+# numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+# symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+# print("Welcome to the PyPassword Generator!")
+# nr_letters = int(input("How many letters would you like in your password?\n"))
+# nr_symbols = int(input(f"How many symbols would you like?\n"))
+# nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+## Easy level : Generate the password in sequence. Letters, then symbols, then numbers.
+# password = ""
+
+# <option 1>
+# for char in range(1, nr_letters + 1):
+#     password += random.choice(letters)
+
+# for char in range(1, nr_symbols + 1):
+#     password += random.choice(symbols)
+
+# for char in range(1, nr_letters + 1):
+#     password += random.choice(numbers)
+
+# print(password)
+
+# for char in range(0, nr_letters):
+#     password += random.choice(letters)
+
+# for char in range(0, nr_symbols):
+#     password += random.choice(symbols)
+
+# for char in range(0, nr_letters):
+#     password += random.choice(numbers)
+
+# print(password)
+
+# <Both of the options have the same outcome>
+
+## Hard level : doesn't follow a pattern.
+# import random
+# letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g','h', 'i', 'j', 'k', 'l', 'm', 'n','o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+# numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+# symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+# print("Welcome to the PyPassword Generator!")
+# nr_letters = int(input("How many letters would you like in your password?\n"))
+# nr_symbols = int(input(f"How many symbols would you like?\n"))
+# nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+
+# password_list = []
+# for char in range(0, nr_letters):
+#     password_list.append(random.choice(letters))
+
+# for char in range(0, nr_symbols):
+#     password_list.append(random.choice(symbols))
+
+# for char in range(0, nr_numbers):
+#     password_list.append(random.choice(numbers))
+
+# print(password_list)
+# random.shuffle(password_list)
+# print(password_list)
+
+# password = ""
+# for char in password_list:
+#     password += char
+
+# print(f"Your password is {password}")
+
+# ----end of Day 5 Project---------------
+
