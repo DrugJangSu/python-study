52f1da184ebad38c480f3d0e74cc782a5e9b0db1
DrugJangSu <ljonghyun6088@gmail.com>
2025-07-21 15:12:43 +0900

    Update

--- 100_days_of_python.py
+++ 100_days_of_python.py
@@ -1288,17 +1288,205 @@
 # greet_with_name("Angela")
 
 # Functions with more than 1 input
-def greet_with(name, location):
-    print(f"Hello {name}")
-    print(f"What is it like in {location}?")
+# def greet_with(name, location):
+#     print(f"Hello {name}")
+#     print(f"What is it like in {location}?")
 
-greet_with("Angela", "London")
-greet_with("Hyun", "Seoul")
+# greet_with("Angela", "London")
+# greet_with("Hyun", "Seoul")
 
-greet_with("Nowhere", "Jack Bauer")
-# The first argument is always the first parameter, and the second argument is always the second parameter.
-# You can also use keyword arguments to sepcify which argument goes to which parameter.
-greet_with(location="Nowhere", name="Jack Bauer")
-# This way you can change the order of the arguments without changing the order of the parameters.
+# greet_with("Nowhere", "Jack Bauer")
+# # The first argument is always the first parameter, and the second argument is always the second parameter.
+# # You can also use keyword arguments to sepcify which argument goes to which parameter.
 
+# greet_with(location="Nowhere", name="Jack Bauer")
+# # This way you can change the order of the arguments without changing the order of the parameters.
+
+
+# def greet_with(name, location):
+#     print(f"Hello {name}")
+#     print(f"What is it like in {location}?")
+
+# greet_with(location="London", name="Angela")
+# # This will print the same output as the previous example(like the example above)
+# greet_with(location="Seoul", name="Hyun")
+
+# # Day 8 final project(Caesar Cipher)
+# Caesar Cipher is a type of substitution cipher where each letter in the plaintext is replaced by a letter some fixed number of positions down or up the alphabet.
+
+# TODO-1 : Create a function called encrypt() that takes original_text and shift_amount as 2 inputs.
+# TODO-2 : Inside the encrypt() function, shift each letter of the 'original_text' forwards in the alphabet by the shift amount and print the encrypted text.
+# TODO-3 : Call the 'encrypt()' function and pass in the user inputs. You should be able to test the code and encrypt a message.
+# TODO-4 : What happens if you try to shift z forwards by 9? Can you fix the code?
+# TODO-5 : Return the encrypted text
+
+
+# """
+#  ,adPPYba, ,adPPYYba,  ,adPPYba, ,adPPYba, ,adPPYYba, 8b,dPPYba,  
+# a8"     "" ""     `Y8 a8P_____88 I8[    "" ""     `Y8 88P'   "Y8  
+# 8b         ,adPPPPP88 8PP"""""""  `"Y8ba,  ,adPPPPP88 88          
+# "8a,   ,aa 88,    ,88 "8b,   ,aa aa    ]8I 88,    ,88 88          
+#  `"Ybbd8"' `"8bbdP"Y8  `"Ybbd8"' `"YbbdP"' `"8bbdP"Y8 88 
+#            88             88                                 
+#            ""             88                                 
+#                           88                                 
+#  ,adPPYba, 88 8b,dPPYba,  88,dPPYba,   ,adPPYba, 8b,dPPYba,  
+# a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8  
+# 8b         88 88       d8 88       88 8PP""""""" 88          
+# "8a,   ,aa 88 88b,   ,a8" 88       88 "8b,   ,aa 88          
+#  `"Ybbd8"' 88 88`YbbdP"'  88       88  `"Ybbd8"' 88          
+#               88                                             
+#               88                                            
+# """
+
+# alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+#             'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+# direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+# text = input("Type your message:\n").lower()
+# shift = int(input("Type the shift number:\n"))
+
+# --------------"Angela's Solution"-------------------------------------------
+## <#1 How to encode>
+# TODO-1 : Create a function called encrypt() that takes original_text and shift_amount as 2 inputs.
+
+# def encrypt(original_text, shift_amount):
+
+# TODO-2 : Inside the encrypt() function, shift each letter of the 'original_text' forwards in the alphabet by the shift amount and print the encrypted text.
+
+
+# def encrypt(original_text, shift_amount):
+#     cipher_text = ""
+
+#     for letter in original_text:
+#         shifted_position = alphabet.index(letter) + shift_amount
+#         cipher_text += alphabet[shifted_position]
+
+# print(f"Here is the encoded result: {cipher_text}")
+
+# TODO-3 : Call the 'encrypt()' function and pass in the user inputs. You should be able to test the code and encrypt a message.
+
+# def encrypt(original_text, shift_amount):
+#     cipher_text = ""
+
+
+
+#     for letter in original_text:
+#         shifted_position = alphabet.index(letter) + shift_amount
+#         cipher_text += alphabet[shifted_position]
+
+#     print(f"Here is the encoded result: {cipher_text}")
+
+# encrypt(original_text=text, shift_amount=shift)
+
+
+# TODO-4 : What happens if you try to shift z forwards by 9? Can you fix the code?
+
+# def encrypt(original_text, shift_amount):
+#     cipher_text = ""
+
+
+#     for letter in original_text:
+#         shifted_position = alphabet.index(letter) + shift_amount
+
+#         shifted_position %= len(alphabet) #0~25 withinrange
+#         cipher_text += alphabet[shifted_position]
+
+#     print(f"Here is the encoded result: {cipher_text}")
+
+# encrypt(original_text=text, shift_amount=shift)
+
+# <#2 How to decode>------------------
+
+# TODO-1 : Create a function called decrypt() that takes 'original_text' and 'shift_amount' as 2 inputs.
+
+# TODO-2 : Inside the decrypt() function, shift each letter of the 'original_text' backwards in the alphabet by the shift amount and print the decrypted text.
+
+# TODO-3 : Combine the 'encrypt()' and 'decrypt()' functions into a single function called caesar().
+         # Use the value of the user chosen direction variable to determine which functionality to use.
+         # Call the ceasar() function instead of encrypt/decrypt and pass in all three variables direction/text/shift.
+
+# """
+#  ,adPPYba, ,adPPYYba,  ,adPPYba, ,adPPYba, ,adPPYYba, 8b,dPPYba,  
+# a8"     "" ""     `Y8 a8P_____88 I8[    "" ""     `Y8 88P'   "Y8  
+# 8b         ,adPPPPP88 8PP"""""""  `"Y8ba,  ,adPPPPP88 88          
+# "8a,   ,aa 88,    ,88 "8b,   ,aa aa    ]8I 88,    ,88 88          
+#  `"Ybbd8"' `"8bbdP"Y8  `"Ybbd8"' `"YbbdP"' `"8bbdP"Y8 88 
+#            88             88                                 
+#            ""             88                                 
+#                           88                                 
+#  ,adPPYba, 88 8b,dPPYba,  88,dPPYba,   ,adPPYba, 8b,dPPYba,  
+# a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8  
+# 8b         88 88       d8 88       88 8PP""""""" 88          
+# "8a,   ,aa 88 88b,   ,a8" 88       88 "8b,   ,aa 88          
+#  `"Ybbd8"' 88 88`YbbdP"'  88       88  `"Ybbd8"' 88          
+#               88                                             
+#               88                                            
+# """
+
+# alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+#             'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+# direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+# text = input("Type your message:\n").lower()
+# shift = int(input("Type the shift number:\n"))
+
+# def encrypt(original_text, shift_amount):
+#     cipher_text = ""
+#     for letter in original_text:
+#         shifted_position = alphabet.index(letter) + shift_amount
+#         shifted_position %= len(alphabet) #0~25 withinrange
+#         cipher_text += alphabet[shifted_position]
+#     print(f"Here is the encoded result: {cipher_text}")
+
+# encrypt(original_text=text, shift_amount=shift)
+
+# # TODO-1 : Create a function called decrypt() that takes 'original_text' and 'shift_amount' as 2 inputs.
+# # TODO-2 : Inside the decrypt() function, shift each letter of the 'original_text' backwards in the alphabet by the shift amount and print the decrypted text.
+
+# def decrypt(original_text, shift_amount):
+#     outout_text = ""
+#     for letter in original_text:
+#         shifted_position = alphabet.index(letter) - shift_amount
+#         shifted_position %= len(alphabet) #0~25 withinrange
+#         outout_text += alphabet[shifted_position]
+#     print(f"Here is the encoded result: {outout_text}")
+
+# decrypt(original_text=text, shift_amount=shift)
+
+# # TODO-3 : Combine the 'encrypt()' and 'decrypt()' functions into a single function called caesar().
+#          # Use the value of the user chosen direction variable to determine which functionality to use.
+#          # Call the ceasar() function instead of encrypt/decrypt and pass in all three variables direction/text/shift.
+
+# def caesar(original_text, shift_amount, encode_or_decode):
+#     outout_text = ""
+#     for letter in original_text:
+
+#         if encode_or_decode == "decode":
+#             shift_amount *= -1 # If the user chooses to decode, we need to reverse the shift amount.
+
+#         shifted_position = alphabet.index(letter) + shift_amount
+#         shifted_position %= len(alphabet) #0~25 withinrange
+#         outout_text += alphabet[shifted_position]
+#     print(f"Here is the encoded result: {outout_text}")
+
+# <current resized code>----------------------------------------------------------
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+            'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+def caesar(original_text, shift_amount, encode_or_decode):
+    outout_text = ""
+    for letter in original_text:
+
+        if encode_or_decode == "decode":
+            shift_amount *= -1 # If the user chooses to decode, we need to reverse the shift amount.
+
+        shifted_position = alphabet.index(letter) + shift_amount
+        shifted_position %= len(alphabet) #0~25 withinrange
+        outout_text += alphabet[shifted_position]
+    print(f"Here is the {encode_or_decode}d result: {outout_text}")
+
+caesar(original_text=text, shift_amount=shift, encode_or_decode=direction)
 
