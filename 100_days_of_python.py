be76e8361146959fb103d529813ba5dd68803e3c
DrugJangSu <ljonghyun6088@gmail.com>
2025-09-21 14:00:40 +0900

    Update

--- 100_days_of_python.py
+++ 100_days_of_python.py
@@ -1747,5 +1747,21 @@
 
 
 # ----end of Day 9 Project--------------
-
 ### Review purposes
+# print("Hello_world")
+# print("Hello" + " " + "Angela")
+
+# # Coding exercise #1 - Printing practice
+# print("1. Mix 500g of Flour, 10g Yeast and 300ml Water in a bowl.")
+# print("2. Knead the dough for 10 minutes.")
+# print("3. Add 3g of Salt.")
+# print("4. Leave to rise for 2 hours.")
+# print("5. Bake at 200 degrees C for 30 minutes.")
+
+
+# # Coding exercise #2 - Debugging practice
+# print("Notes from Day 1")
+# print("The print statement is used to output strings")
+# print("Strings are strings of characters")
+# print("String Concatenation is done with the + sign")
+# print(("New lines can be created with a \ and the letter n"))
