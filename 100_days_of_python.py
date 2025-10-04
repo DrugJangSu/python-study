ef504dd7c5fa89eb88695b0a987ce6fcc9721af8
DrugJangSu <ljonghyun6088@gmail.com>
2025-10-04 14:57:58 +0900

    Update

--- 100_days_of_python.py
+++ 100_days_of_python.py
@@ -2001,22 +2001,22 @@ extra_cheese = input("Do you want extra cheese? Y or N \n")
 # Pepperoni for Medium or Large pizza : +$3
 # Extra cheese for any size pizza : + $1
 
-bill = 0
-if size == "S":
-    bill += 15
-elif size == "M":
-    bill += 20
-elif size == "L":
-    bill += 25
-else:
-    print("You typed the wrong inputs. Please enter the right size.")
+# bill = 0
+# if size == "S":
+#     bill += 15
+# elif size == "M":
+#     bill += 20
+# elif size == "L":
+#     bill += 25
+# else:
+#     print("You typed the wrong inputs. Please enter the right size.")
 
-if pepperoni == "Y":
-    if size == "S":
-        bill += 2
-    else:
-        bill += 3
-if extra_cheese == "Y":
-    bill += 1
+# if pepperoni == "Y":
+#     if size == "S":
+#         bill += 2
+#     else:
+#         bill += 3
+# if extra_cheese == "Y":
+#     bill += 1
 
-print(f"Your final bill is: ${bill}.")
+# print(f"Your final bill is: ${bill}.")
