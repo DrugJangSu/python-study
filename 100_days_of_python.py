f5313a4cc59a5f3412dab54d567062ccb0f6e97a
DrugJangSu <ljonghyun6088@gmail.com>
2025-10-06 15:54:31 +0900

    Update

--- 100_days_of_python.py
+++ 100_days_of_python.py
@@ -2173,12 +2173,20 @@ student_scores= [150, 142, 185, 120, 171, 184, 149, 24, 59, 68, 91, 111, 178, 16
 # print(sum)
 
 # # how to fnd the highest score
-max = 0
-for score in student_scores:
-    if score > max:
-        max = score
+# max = 0
+# for score in student_scores:
+#     if score > max:
+#         max = score
 
-print(f"The highest score in the class is: {max}")
+# print(f"The highest score in the class is: {max}")
+
+# Angela's solution
+
+max_score = 0
+for score in student_scores:
+    if score > max_score:
+        max_score = score
+print(f"The highest score in the class is: {max_score}")
 
 
 
