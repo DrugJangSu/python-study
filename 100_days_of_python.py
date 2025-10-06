d5a3f99ae19a0c0a9e50cf412d60e0bd0a390016
DrugJangSu <ljonghyun6088@gmail.com>
2025-10-06 20:41:30 +0900

    Update

--- 100_days_of_python.py
+++ 100_days_of_python.py
@@ -2355,3 +2355,21 @@ my_function() #this is how to execute the defined function, without any print st
 # for step in range(0, 6):
 #     jump()
 
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+def jump():
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+
+
+number_of_hurdles = 6
+while number_of_hurdles >0:
+    jump()
