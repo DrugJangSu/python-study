067b0caa7b868194a005ecce0b0ed8830d7dcfb3
DrugJangSu <ljonghyun6088@gmail.com>
2025-10-06 20:43:50 +0900

    Update

--- 100_days_of_python.py
+++ 100_days_of_python.py
@@ -2373,3 +2373,5 @@ my_function() #this is how to execute the defined function, without any print st
 # number_of_hurdles = 6
 # while number_of_hurdles >0:
 #     jump()
+#     number_of_hurdles -= 1
+#     print(number_of_hurdles)
