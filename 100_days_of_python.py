3b3e2e010ab5544260c6dcd36d669f942a111359
DrugJangSu <ljonghyun6088@gmail.com>
2025-10-07 20:02:25 +0900

    Update

--- 100_days_of_python.py
+++ 100_days_of_python.py
@@ -2440,3 +2440,25 @@ my_function() #this is how to execute the defined function, without any print st
 #         jump()
 #     else:
 #         move()
+
+# <Final project review : Maze>
+# <My solution>
+
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+    
+# while not at_goal():
+#     if front_is_clear() and wall_on_right():
+#         move()
+#     elif front_is_clear() and right_is_clear():
+#         turn_right()
+#         move()
+#     elif wall_in_front() and wall_on_right():
+#         turn_left()
+#     elif wall_in_front and right_is_clear():
+#         turn_right()
+#         move()
+
+## Success without any errors!
