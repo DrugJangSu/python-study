302e6993443529399a960f27ab7966b84455801b
DrugJangSu <ljonghyun6088@gmail.com>
2025-11-01 16:48:20 +0900

    Update

--- 100_days_of_python.py
+++ 100_days_of_python.py
@@ -3304,8 +3304,29 @@ travel_log = {
     "France": ["Paris", "Lille", "Dijon"],
     "Germany": ["Berlin", "Hamburg", "Stuttgart"],
 }
-
-#print Lille
+#If I want to print Lille
 print(travel_log["France"])
 print(travel_log["France"][1])
+
+nested_list = ["A", "B", ["C", "D"]]
+#If I want to print D
+print(nested_list[2][1])
+
+## This is a list nested inside a dictionary which is nested inside another dictionary.
+travel_log = {
+    "France": {
+        "cities_visited": ["Paris", "Lille", "Dijon"],
+        "total_visits": 12
+    },    
+    "Germany": {
+    "cities_visited": ["Berlin", "Hamburg", "Stuttgart"],
+    "total_visits": 5
+    },
+}
+
+# If I want to print out Stuttgart from this travel_log
+print(travel_log["Germany"]["cities_visited"][2])
+
+
+
 
