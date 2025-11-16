b59d7d985bd39645a9af154b8d0a28c9cbd50003
DrugJangSu <ljonghyun6088@gmail.com>
2025-11-16 20:54:15 +0900

    Update

--- 100_days_of_python.py
+++ 100_days_of_python.py
@@ -3574,11 +3574,88 @@
 
 # """ -> this is a doc"""
 
-## Day 10 project - The Calculator Project
+## Day 10 project - The Calculator Project ##
 
-def add(n1, n2):
-    return n1 + n2
+# TODO-1: Write out the other 3 functions - subtract, multiply and divide
 
-my_favorite_operation = add
+# def add(n1, n2): # 덧셈
+#     return n1 + n2
 
-my_favorite_operation()
+# def multiply(n1, n2): # 뺄셈
+#     return n1 * n2
+
+# def subtract(n1, n2): #곱셈
+#     return n1 - n2
+
+# def divide(n1, n2): #나눗셈
+#     return n1 / n2
+
+# # TODO-2: Add these 4 functions into a dictionary as the values. Keys = "+", "-", "*", "/"
+# operations = {"+" : add, "-" : subtract, "*" : multiply, "/" : divide,}
+# ## Remember not to trigger the functions because we're storing it and not using it, thus we don't use parentheses. This is literally a dictonary that stores functions as values.
+
+# # TODO-3: Use the dictionary ooperations to perform the calculations. (multiply 4 * 8 using the dictonary)
+
+# print(operations["*"](n1 = 4, n2 = 8))
+
+# # <This is the basic structure of the calculator app>
+
+# <Angela's solution>--------------------------------------------
+
+# art_logo = """
+#  _____________________
+# |  _________________  |
+# | | Jong Hyun.      | |  .----------------.  .----------------.  .----------------.  .----------------.
+# | |_________________| | | .--------------. || .--------------. || .--------------. || .--------------. |
+# |  ___ ___ ___   ___  | | |     ______   | || |      __      | || |   _____      | || |     ______   | |
+# | | 7 | 8 | 9 | | + | | | |   .' ___  |  | || |     /  \     | || |  |_   _|     | || |   .' ___  |  | |
+# | |___|___|___| |___| | | |  / .'   \_|  | || |    / /\ \    | || |    | |       | || |  / .'   \_|  | |
+# | | 4 | 5 | 6 | | - | | | |  | |         | || |   / ____ \   | || |    | |   _   | || |  | |         | |
+# | |___|___|___| |___| | | |  \ `.___.'\  | || | _/ /    \ \_ | || |   _| |__/ |  | || |  \ `.___.'\  | |
+# | | 1 | 2 | 3 | | x | | | |   `._____.'  | || ||____|  |____|| || |  |________|  | || |   `._____.'  | |
+# | |___|___|___| |___| | | |              | || |              | || |              | || |              | |
+# | | . | 0 | = | | / | | | '--------------' || '--------------' || '--------------' || '--------------' |
+# | |___|___|___| |___| |  '----------------'  '----------------'  '----------------'  '----------------'
+# |_____________________|
+# """
+
+# def add(n1, n2): 
+#     return n1 + n2
+
+# def multiply(n1, n2): 
+#     return n1 * n2
+
+# def subtract(n1, n2):
+#     return n1 - n2
+
+# def divide(n1, n2): 
+#     return n1 / n2
+
+# operations = {"+" : add, "-" : subtract, "*" : multiply, "/" : divide,}
+
+# def calculator():
+#     print(art_logo)
+#     should_accmulate = True
+#     num1 = float(input("What is the first number?: "))
+
+#     while should_accmulate:
+#         for symbol in operations:
+#             print(symbol)
+#         operation_symbol = input("Pick an operation: ")
+#         num2 = float(input("What is the next number?: "))
+#         answer = operations[operation_symbol](num1, num2)
+#         print(f"{num1} {operation_symbol} {num2} = {answer}")
+
+#         choice = input("Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation.: ")
+
+#         if choice == 'y':
+#             num1 = answer
+#         else:
+#             should_accmulate = False
+#             print("\n" * 20)
+#             calculator() # Restarting the calculator function- this is called recursion
+
+# calculator()
+
+# --------------<end of Day 10>------------------------
+
