80ea5cf82a386f9eefdc32e60bfd173aa1c44efc
DrugJangSu <ljonghyun6088@gmail.com>
2025-11-17 20:06:43 +0900

    Update

--- 100_days_of_python.py
+++ 100_days_of_python.py
@@ -3670,6 +3670,106 @@
 # Ace is worth 11 or 1
 # The deck is unlimited in size. There are no jokers. The cards in the deck have equal probability of being drawn.
 
-cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+# cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 # four 10s represent 10, J, Q, K
 # 11 represents Ace, until the number goes above 21 which it becomes 1.
+
+## <Angela's Solution>-----------------------------
+## ----<Hint 4 and 5>----
+
+# import random
+
+## Hint 4 : Create a function deal_card() that uses the random module to return a random card from the list of cards.
+
+# def deal_card():
+#     """Returns a random card from the deck."""
+#     cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+#     card = random.choice(cards)
+#     return card
+## Hint 5 : Deal with the user and computer 2 cards each using deal-card() and append().
+
+# user_cards = []
+# computer_cards = []
+
+# for _ in range(2):
+#     user_cards.append(deal_card())
+#     computer_cards.append(deal_card())
+
+
+### Current code status ###
+import random
+def deal_card():
+    """Returns a random card from the deck."""
+    cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+    card = random.choice(cards)
+    return card
+
+user_cards = []
+computer_cards = []
+
+for _ in range(2):
+    user_cards.append(deal_card())
+    computer_cards.append(deal_card())
+
+## Hint 6 : Create a function calculate_score() that takes a List of cards as input and returns the score. Use the sum() function to get the score.
+
+# def calculate_score(cards):
+#     return sum(cards)
+
+## Hint 7 : Inside calculate_score() check for a blackjack (a hand with only 2 cards: ace + 10) and return 0 instead of the actual score. 0 will represent a blackjack in our game.
+
+
+# def calculate_score(cards):
+#     if sum(cards) == 21 and len(cards) == 2:
+#         return 0
+
+#     return sum(cards)
+
+## Hint 8 : Inside calculate_score() check for an 11 (ace). If the score is over 21, remove the 11 and replace it with a 1. You might need to look up append() and remove().
+
+# def calculate_score(cards):
+#     if sum(cards) == 21 and len(cards) == 2:
+#         return 0
+    
+#     if 11 in cards and sum(cards) < 21:
+#         cards.remove(11)
+#         cards.append(1)
+    
+#     return sum(cards)
+
+### Current code status ###
+import random
+def deal_card():
+    """Returns a random card from the deck."""
+    cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+    card = random.choice(cards)
+    return card
+
+def calculate_score(cards):
+    if sum(cards) == 21 and len(cards) == 2:
+        return 0
+    
+    if 11 in cards and sum(cards) < 21:
+        cards.remove(11)
+        cards.append(1)
+    
+    return sum(cards)
+
+user_cards = []
+computer_cards = []
+is_game_over = False
+
+for _ in range(2):
+    user_cards.append(deal_card())
+    computer_cards.append(deal_card())
+
+## Hint 9 Call the function calculate_score(). If the computer or the user has a blackjack (0) or if the user's score is over 21, then the game ends.
+
+user_score = calculate_score(user_cards)
+computer_score = calculate_score(computer_cards)
+print(f"Your cards: {user_cards}, current score: {user_score}")
+print(f"Computer's first card: {computer_cards[0]}")
+
+
+if user_score == 0 or computer_score == 0 or user_score > 21:
+    is_game_over = True
