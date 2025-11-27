427c228bd884cda5e7e8d0bd1d4f2a75b7c9a98f
DrugJangSu <ljonghyun6088@gmail.com>
2025-11-27 17:54:00 +0900

    Update

--- 100_days_of_python.py
+++ 100_days_of_python.py
@@ -5921,7 +5921,40 @@
 # print(table)
 
 ## Building the Coffee Machine with OOP -------------
+# <for proper documentation and exercise, look up the folder oop-coffee-machine-start's main.py file>
+
+# from menu import Menu
+# from menu import MenuItem
+# from coffee_maker import CoffeeMaker
+# from money_machine import MoneyMachine
+
+# # TODO-1 :  print report
+
+# money_machine = MoneyMachine()
+# coffee_maker = CoffeeMaker()
+# menu = Menu()
 
 
+# money_machine.report()
+# coffee_maker.report()
+
+# # TODO-2 : check resources sufficient
+# # TODO-3 : process coins
+# # TODO-4 : Check transaction successful?
+# is_on = True
+# while is_on:
+#     options = menu.get_items()
+#     choice = input(f"What would you like? ({options})")
+#     if choice == "off":
+#         is_on = False
+#     elif choice == "report":
+#         coffee_maker.report()
+#         money_machine.report()
+#     else:
+#         drink = menu.find_drink(choice)
+#         if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+#             coffee_maker.make_coffee(drink)
 
 
+# --------End of day 16--------------
+# Day 17 : Intermediate - The Quiz Project & The Benefits of OOP
