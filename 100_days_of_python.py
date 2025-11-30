6f1dc62677880872e611b18cf4c0cd1bea6e2bb5
DrugJangSu <ljonghyun6088@gmail.com>
2025-11-30 13:35:32 +0900

    Update

--- 100_days_of_python.py
+++ 100_days_of_python.py
@@ -6323,3 +6323,15 @@
 # ## -------End of day 18--------------
 
 ## Day 19 : Intermediate - Instances, State and Higher Order Functions
+
+from turtle import Turtle, Screen
+
+tim = Turtle()
+screen = Screen()
+
+def move_forwards():
+    tim.forward(10)
+
+screen.listen()
+screen.onkey(key="space", fun=move_forwards)
+screen.exitonclick()
