eb1b55529e489b5e134c0933571bd2fdbc0301df
DrugJangSu <ljonghyun6088@gmail.com>
2025-12-07 19:40:23 +0900

    Update

--- 100_days_of_python.py
+++ 100_days_of_python.py
@@ -7401,7 +7401,540 @@
 # screen.exitonclick()
 
 ## Create another paddle
+# Paddle.py
+
+# from turtle import Turtle
+
+# class Paddle(Turtle):
+
+#     def __init__(self, position):
+#         super().__init__()
+#         self.shape("square")
+#         self.color("white")
+#         self.shapesize(stretch_wid=5, stretch_len=1)
+#         self.penup()
+#         self.goto(position)
+
+#     def go_up(self):
+#         new_y = self.ycor() + 20
+#         self.goto(self.xcor(), new_y)
+
+#     def go_down(self):
+#         new_y = self.ycor() - 20
+#         self.goto(self.xcor(), new_y)
+
+# main.py
+# from turtle import Screen, Turtle
+# from paddle import Paddle
+
+# screen = Screen()
+# screen.bgcolor("black")
+# screen.setup(width=800, height=600)
+# screen.title("Pong")
+# screen.tracer(0)
+
+# r_paddle = Paddle((350, 0))
+# l_paddle = Paddle((-350, 0))
 
 
+# screen.listen()
+# screen.onkey(r_paddle.go_up, "Up")
+# screen.onkey(r_paddle.go_down, "Down")
+# screen.onkey(l_paddle.go_up, "w")
+# screen.onkey(l_paddle.go_down, "s")
+
+
+# game_is_on = True
+# while game_is_on:
+#     screen.update()
+ 
+
+# screen.exitonclick()
+
+## Create the ball and make it move
+
+# ball.py
+# from turtle import Turtle
+
+# class Ball(Turtle):
+#     def __init__(self):
+#         super().__init__()
+#         self.color("White")
+#         self.shape("circle")
+#         self.penup()
+    
+#     def move(self):
+#         new_x = self.xcor() + 10
+#         new_y = self.ycor() + 10
+#         self.goto(new_x, new_y)
+
+# main.py
+# from turtle import Screen, Turtle
+# from paddle import Paddle
+# from ball import Ball
+# import time
+
+
+# screen = Screen()
+# screen.bgcolor("black")
+# screen.setup(width=800, height=600)
+# screen.title("Pong")
+# screen.tracer(0)
+
+# r_paddle = Paddle((350, 0))
+# l_paddle = Paddle((-350, 0))
+# ball = Ball()
+
+# screen.listen()
+# screen.onkey(r_paddle.go_up, "Up")
+# screen.onkey(r_paddle.go_down, "Down")
+# screen.onkey(l_paddle.go_up, "w")
+# screen.onkey(l_paddle.go_down, "s")
+
+
+# game_is_on = True
+# while game_is_on:
+#     time.sleep(0.1)
+#     screen.update()
+#     ball.move()
+
+# screen.exitonclick()
+
+
+## Detect collision with wall and bounce
+
+## ball.py
+# from turtle import Turtle
+
+# class Ball(Turtle):
+#     def __init__(self):
+#         super().__init__()
+#         self.color("White")
+#         self.shape("circle")
+#         self.penup()
+#         self.x_move = 10
+#         self.y_move = 10
+    
+#     def move(self):
+#         new_x = self.xcor() + self.x_move
+#         new_y = self.ycor() + self.y_move
+#         self.goto(new_x, new_y)
+
+
+#     def bounce(self):
+#         self.y_move *= -1
+
+# main.py
+# from turtle import Screen, Turtle
+# from paddle import Paddle
+# from ball import Ball
+# import time
+
+
+# screen = Screen()
+# screen.bgcolor("black")
+# screen.setup(width=800, height=600)
+# screen.title("Pong")
+# screen.tracer(0)
+
+# r_paddle = Paddle((350, 0))
+# l_paddle = Paddle((-350, 0))
+# ball = Ball()
+
+# screen.listen()
+# screen.onkey(r_paddle.go_up, "Up")
+# screen.onkey(r_paddle.go_down, "Down")
+# screen.onkey(l_paddle.go_up, "w")
+# screen.onkey(l_paddle.go_down, "s")
+
+
+# game_is_on = True
+# while game_is_on:
+#     time.sleep(0.1)
+#     screen.update()
+#     ball.move()
+
+#     # Detect collision with wall
+#     if ball.ycor() > 280 or ball.ycor() < -280:
+#     # needs to bounce lol
+#         ball.bounce()
+
+# screen.exitonclick()
+
+## Detect collision with the paddle
+# Paddle.py
+# from turtle import Turtle
+
+
+# class Paddle(Turtle):
+
+#     def __init__(self, position):
+#         super().__init__()
+#         self.shape("square")
+#         self.color("white")
+#         self.shapesize(stretch_wid=5, stretch_len=1)
+#         self.penup()
+#         self.goto(position)
+
+#     def go_up(self):
+#         new_y = self.ycor() + 20
+#         self.goto(self.xcor(), new_y)
+
+#     def go_down(self):
+#         new_y = self.ycor() - 20
+#         self.goto(self.xcor(), new_y)
+
+
+# main.py
+# from turtle import Screen, Turtle
+# from paddle import Paddle
+# from ball import Ball
+# import time
+
+
+# screen = Screen()
+# screen.bgcolor("black")
+# screen.setup(width=800, height=600)
+# screen.title("Pong")
+# screen.tracer(0)
+
+# r_paddle = Paddle((350, 0))
+# l_paddle = Paddle((-350, 0))
+# ball = Ball()
+
+# screen.listen()
+# screen.onkey(r_paddle.go_up, "Up")
+# screen.onkey(r_paddle.go_down, "Down")
+# screen.onkey(l_paddle.go_up, "w")
+# screen.onkey(l_paddle.go_down, "s")
+
+
+# game_is_on = True
+# while game_is_on:
+#     time.sleep(0.1)
+#     screen.update()
+#     ball.move()
+
+#     # Detect collision with wall
+#     if ball.ycor() > 280 or ball.ycor() < -280:
+#     # needs to bounce lol
+#         ball.bounce_y()
+    
+#     # Detect collision with paddle
+#     if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+#         ball.bounce_x()
+
+# screen.exitonclick()
+
+
+## Detect when paddle misses
+# ball.py
+
+# from turtle import Turtle
+
+# class Ball(Turtle):
+#     def __init__(self):
+#         super().__init__()
+#         self.color("White")
+#         self.shape("circle")
+#         self.penup()
+#         self.x_move = 10
+#         self.y_move = 10
+    
+#     def move(self):
+#         new_x = self.xcor() + self.x_move
+#         new_y = self.ycor() + self.y_move
+#         self.goto(new_x, new_y)
+
+
+#     def bounce_y(self):
+#         self.y_move *= -1
+
+#     def bounce_x(self):
+#         self.x_move *= -1
+    
+#     def reset_position(self):
+#         self.goto(0, 0)
+#         self.bounce_x()
+    
+
+# main.py
+# from turtle import Screen, Turtle
+# from paddle import Paddle
+# from ball import Ball
+# import time
+
+
+# screen = Screen()
+# screen.bgcolor("black")
+# screen.setup(width=800, height=600)
+# screen.title("Pong")
+# screen.tracer(0)
+
+# r_paddle = Paddle((350, 0))
+# l_paddle = Paddle((-350, 0))
+# ball = Ball()
+
+# screen.listen()
+# screen.onkey(r_paddle.go_up, "Up")
+# screen.onkey(r_paddle.go_down, "Down")
+# screen.onkey(l_paddle.go_up, "w")
+# screen.onkey(l_paddle.go_down, "s")
+
+
+# game_is_on = True
+# while game_is_on:
+#     time.sleep(0.1)
+#     screen.update()
+#     ball.move()
+
+#     # Detect collision with wall
+#     if ball.ycor() > 280 or ball.ycor() < -280:
+#     # needs to bounce lol
+#         ball.bounce_y()
+    
+#     # Detect collision with paddle
+#     if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+#         ball.bounce_x()
+    
+#     # Detect R paddle misses
+#     if ball.xcor() > 380:
+#         ball.reset_position()
+
+#     # Detect L paddle misses
+#     if ball.xcor() < -380:
+#         ball.reset_position()
+
+
+# screen.exitonclick()
+
+## Score Keeping and Changing the Ball Speed
+
+# scoreboard.py
+# from turtle import Turtle
+
+# class Scoreboard(Turtle):
+
+#     def __init__(self):
+#         super().__init__()
+#         self.color("white")
+#         self.penup()
+#         self.hideturtle()
+#         self.l_score = 0
+#         self.r_score = 0
+#         self.update_scoreboard()
+
+#     def update_scoreboard(self):
+#         self.clear()
+#         self.goto(-180, 200)
+#         self.write(self.l_score, align="center", font=("Courier", 80, "normal"))
+#         self.goto(180, 200)
+#         self.write(self.r_score, align="center", font=("Courier", 80, "normal"))
+
+#     def l_point(self):
+#         self.l_score += 1
+#         self.update_scoreboard()
+    
+#     def r_point(self):
+#         self.r_score += 1
+#         self.update_scoreboard()
+
+# main.py
+
+# from turtle import Screen, Turtle
+# from paddle import Paddle
+# from ball import Ball
+# from scoreboard import Scoreboard
+# import time
+
+
+# screen = Screen()
+# screen.bgcolor("black")
+# screen.setup(width=800, height=600)
+# screen.title("Pong")
+# screen.tracer(0)
+
+# r_paddle = Paddle((350, 0))
+# l_paddle = Paddle((-350, 0))
+# ball = Ball()
+# scoreboard = Scoreboard()
+
+# screen.listen()
+# screen.onkey(r_paddle.go_up, "Up")
+# screen.onkey(r_paddle.go_down, "Down")
+# screen.onkey(l_paddle.go_up, "w")
+# screen.onkey(l_paddle.go_down, "s")
+
+
+# game_is_on = True
+# while game_is_on:
+#     time.sleep(0.1)
+#     screen.update()
+#     ball.move()
+
+#     # Detect collision with wall
+#     if ball.ycor() > 280 or ball.ycor() < -280:
+#     # needs to bounce lol
+#         ball.bounce_y()
+    
+#     # Detect collision with paddle
+#     if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+#         ball.bounce_x()
+    
+#     # Detect R paddle misses
+#     if ball.xcor() > 380:
+#         ball.reset_position()
+#         scoreboard.l_point()
+
+#     # Detect L paddle misses
+#     if ball.xcor() < -380:
+#         ball.reset_position()
+#         scoreboard.r_point()
+
+
+# screen.exitonclick()
+
+## Increase the speed & final code
+## FINAL CODE
+## ball.py
+
+# from turtle import Turtle
+
+# class Ball(Turtle):
+#     def __init__(self):
+#         super().__init__()
+#         self.color("White")
+#         self.shape("circle")
+#         self.penup()
+#         self.x_move = 10
+#         self.y_move = 10
+#         self.move_speed = 0.1
+    
+#     def move(self):
+#         new_x = self.xcor() + self.x_move
+#         new_y = self.ycor() + self.y_move
+#         self.goto(new_x, new_y)
+
+
+#     def bounce_y(self):
+#         self.y_move *= -1
+
+#     def bounce_x(self):
+#         self.x_move *= -1
+#         self.move_speed * 0.9
+    
+#     def reset_position(self):
+#         self.goto(0, 0)
+#         self.move_speed = 0.1
+#         self.bounce_x()
+    
+## paddle.py
+
+# from turtle import Turtle
+
+
+# class Paddle(Turtle):
+
+#     def __init__(self, position):
+#         super().__init__()
+#         self.shape("square")
+#         self.color("white")
+#         self.shapesize(stretch_wid=5, stretch_len=1)
+#         self.penup()
+#         self.goto(position)
+
+#     def go_up(self):
+#         new_y = self.ycor() + 20
+#         self.goto(self.xcor(), new_y)
+
+#     def go_down(self):
+#         new_y = self.ycor() - 20
+#         self.goto(self.xcor(), new_y)
+
+## scoreboard.py
+
+# from turtle import Turtle
+
+# class Scoreboard(Turtle):
+
+#     def __init__(self):
+#         super().__init__()
+#         self.color("white")
+#         self.penup()
+#         self.hideturtle()
+#         self.l_score = 0
+#         self.r_score = 0
+#         self.update_scoreboard()
+
+#     def update_scoreboard(self):
+#         self.clear()
+#         self.goto(-180, 200)
+#         self.write(self.l_score, align="center", font=("Courier", 80, "normal"))
+#         self.goto(180, 200)
+#         self.write(self.r_score, align="center", font=("Courier", 80, "normal"))
+
+#     def l_point(self):
+#         self.l_score += 1
+#         self.update_scoreboard()
+    
+#     def r_point(self):
+#         self.r_score += 1
+#         self.update_scoreboard()
+
+## main.py
+# from turtle import Screen, Turtle
+# from paddle import Paddle
+# from ball import Ball
+# from scoreboard import Scoreboard
+# import time
+
+
+# screen = Screen()
+# screen.bgcolor("black")
+# screen.setup(width=800, height=600)
+# screen.title("Pong")
+# screen.tracer(0)
+
+# r_paddle = Paddle((350, 0))
+# l_paddle = Paddle((-350, 0))
+# ball = Ball()
+# scoreboard = Scoreboard()
+
+# screen.listen()
+# screen.onkey(r_paddle.go_up, "Up")
+# screen.onkey(r_paddle.go_down, "Down")
+# screen.onkey(l_paddle.go_up, "w")
+# screen.onkey(l_paddle.go_down, "s")
+
+
+# game_is_on = True
+# while game_is_on:
+#     time.sleep(ball.move_speed)
+#     screen.update()
+#     ball.move()
+
+#     # Detect collision with wall
+#     if ball.ycor() > 280 or ball.ycor() < -280:
+#     # needs to bounce lol
+#         ball.bounce_y()
+    
+#     # Detect collision with paddle
+#     if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+#         ball.bounce_x()
+    
+#     # Detect R paddle misses
+#     if ball.xcor() > 380:
+#         ball.reset_position()
+#         scoreboard.l_point()
+
+#     # Detect L paddle misses
+#     if ball.xcor() < -380:
+#         ball.reset_position()
+#         scoreboard.r_point()
+
+
+# screen.exitonclick()
+## End of Day 22 ------------------------
+## Day 23 - Intermediate - The Turtle Crossing Capstone Project
 
 
