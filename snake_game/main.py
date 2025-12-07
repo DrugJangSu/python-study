1702cd59abe711e2047d60ce09f742e4bf74dd0b
DrugJangSu <ljonghyun6088@gmail.com>
2025-12-07 14:13:32 +0900

    Update

--- snake_game/main.py
+++ snake_game/main.py
@@ -11,6 +11,14 @@ screen.tracer(0)
 
 snake = Snake()
 
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+
+
 screen.update()
 
 game_is_on = True
@@ -22,3 +30,4 @@ while game_is_on:
 
 
 screen.exitonclick()
+
