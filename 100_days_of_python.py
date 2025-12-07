82d0381583b9ca6715d2ac2b3f222fbdc9a2fef1
DrugJangSu <ljonghyun6088@gmail.com>
2025-12-07 18:23:35 +0900

    Update

--- 100_days_of_python.py
+++ 100_days_of_python.py
@@ -7351,3 +7351,51 @@
 ## End of Day 21----------------------------------
 
 ## Day 22 - Intermediate - Build Pong : The famous Arcade Game
+
+## Create the screen
+# main.py
+# from turtle import Screen
+
+# screen = Screen()
+# screen.bgcolor("black")
+# screen.setup(width=800, height=600)
+# screen.title("Pong")
+
+# screen.exitonclick()
+
+## Create and move a paddle
+# from turtle import Screen, Turtle
+
+# screen = Screen()
+# screen.bgcolor("black")
+# screen.setup(width=800, height=600)
+# screen.title("Pong")
+# screen.tracer(0)
+
+# paddle = Turtle()
+# paddle.shape("square")
+# paddle.color("white")
+# paddle.shapesize(stretch_wid=5, stretch_len=1)
+# paddle.penup()
+# paddle.goto(350, 0)
+
+# def go_up():
+#     new_y = paddle.ycor() + 20
+#     paddle.goto(paddle.xcor(), new_y)
+
+# def go_down():
+#     new_y = paddle.ycor() - 20
+#     paddle.goto(paddle.xcor(), new_y)
+
+
+# screen.listen()
+# screen.onkey(go_up, "Up")
+# screen.onkey(go_down, "Down")
+
+# game_is_on = True
+# while game_is_on:
+#     screen.update()
+ 
+# screen.exitonclick()
+
+
