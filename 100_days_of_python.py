96ae8442b5e16fd799a296cfec26d317df6ef432
DrugJangSu <ljonghyun6088@gmail.com>
2025-12-07 13:59:21 +0900

    Update

--- 100_days_of_python.py
+++ 100_days_of_python.py
@@ -6583,5 +6583,58 @@
 
 # screen.exitonclick()
 
-## 3) Control the snake(keyboard controls)
+## 3) Create a Snake Class & Move to OOP
+
+
+## The following is the snake.py (Snake class)
+# from turtle import Turtle
+# STARTING_POSITIONS =[(0, 0), (-20, 0), (-40, 0)]
+# MOVE_DISTANCE = 20
+
+# class Snake:
+#     def __init__(self):
+#         self.segments = []
+#         self.create_snake()
+    
+#     def create_snake(self):
+#         for position in STARTING_POSITIONS:
+#             new_segment = Turtle("square")
+#             new_segment.color("white")
+#             new_segment.penup()
+#             new_segment.goto(position)
+#             self.segments.append(new_segment)
+    
+#     def move(self):
+#         for seg_num in range(len(self.segments) - 1, 0, -1): #(start, stop, step)
+#             new_x = self.segments[seg_num - 1].xcor()
+#             new_y = self.segments[seg_num - 1].ycor()
+#             self.segments[seg_num].goto(new_x, new_y)
+#         self.segments[0].forward(MOVE_DISTANCE)
+
+## And the following is the main.py
+
+# from turtle import Screen, Turtle
+# from snake import Snake
+# import time
+
+# screen = Screen()
+# screen.setup(width = 600, height = 600)
+# screen.bgcolor("black")
+# screen.title("My Snake Game")
+# screen.tracer(0)
+
+
+# snake = Snake()
+
+# screen.update()
+
+# game_is_on = True
+# while game_is_on:
+#     screen.update()
+#     time.sleep(0.1)
+
+#     snake.move()
+
+
+# screen.exitonclick()
 
