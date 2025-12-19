4932acb15048eff0a0371eb1f281fd502702aa69
DrugJangSu <ljonghyun6088@gmail.com>
2025-12-19 13:31:59 +0900

    Update

--- turtle-crossing-start/main.py
+++ turtle-crossing-start/main.py
@@ -9,6 +9,7 @@ screen.setup(width=600, height=600)
 screen.tracer(0)
 
 player = Player()
+car_manager = CarManager()
 
 
 screen.listen()
@@ -19,3 +20,15 @@ game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
+    car_manager.create_car()
+    car_manager.move_cars()
+
+    # Detect collision with car
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20: # collision
+            game_is_on = False
+
+
+screen.exitonclick()
+
