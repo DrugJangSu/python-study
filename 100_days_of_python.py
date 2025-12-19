9d830869856cfc57e2d417729efdf6835a482ad2
DrugJangSu <ljonghyun6088@gmail.com>
2025-12-19 13:54:48 +0900

    Update

--- 100_days_of_python.py
+++ 100_days_of_python.py
@@ -7968,3 +7968,159 @@
 
 # Create a scoreboard that keeps track of which level the user is on. Every time the turtle player does a successful crossing, the level should increase. When the turtle hits a car, GAME OVER should be displayed in the centre. If you get stuck, check the video walkthrough in Step 7.
 
+# # [main.py]
+# import time
+# from turtle import Screen
+# from player import Player
+# from car_manager import CarManager
+# from scoreboard import Scoreboard
+
+# screen = Screen()
+# screen.setup(width=600, height=600)
+# screen.tracer(0)
+
+# player = Player()
+# car_manager = CarManager()
+# scoreboard = Scoreboard()
+
+
+
+# screen.listen()
+# screen.onkey(player.go_up, "Up")
+
+
+# game_is_on = True
+# while game_is_on:
+#     time.sleep(0.1)
+#     screen.update()
+
+#     car_manager.create_car()
+#     car_manager.move_cars()
+
+#     # Detect collision with car
+#     for car in car_manager.all_cars:
+#         if car.distance(player) < 20: # collision
+#             game_is_on = False
+#             scoreboard.game_over()
+
+#     # Detect successful crossing
+#     if player.is_at_finish_line():
+#         player.go_to_start()
+#         car_manager.level_up()
+#         scoreboard.increase_level()
+
+
+# screen.exitonclick()
+
+## [player.py]
+# import time
+# from turtle import Screen
+# from player import Player
+# from car_manager import CarManager
+# from scoreboard import Scoreboard
+
+# screen = Screen()
+# screen.setup(width=600, height=600)
+# screen.tracer(0)
+
+# player = Player()
+# car_manager = CarManager()
+# scoreboard = Scoreboard()
+
+
+
+# screen.listen()
+# screen.onkey(player.go_up, "Up")
+
+
+# game_is_on = True
+# while game_is_on:
+#     time.sleep(0.1)
+#     screen.update()
+
+#     car_manager.create_car()
+#     car_manager.move_cars()
+
+#     # Detect collision with car
+#     for car in car_manager.all_cars:
+#         if car.distance(player) < 20: # collision
+#             game_is_on = False
+#             scoreboard.game_over()
+
+#     # Detect successful crossing
+#     if player.is_at_finish_line():
+#         player.go_to_start()
+#         car_manager.level_up()
+#         scoreboard.increase_level()
+
+
+# screen.exitonclick()
+
+## [car_manager.py]
+
+# from turtle import Turtle
+# import random
+
+# COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
+# STARTING_MOVE_DISTANCE = 5
+# MOVE_INCREMENT = 10
+
+
+# class CarManager:
+    
+#     def __init__(self):
+#         self.all_cars = []
+#         self.car_speed = STARTING_MOVE_DISTANCE
+
+#     def create_car(self):
+#         random_chance = random.randint(1, 6)
+#         if random_chance == 1:
+#             new_car = Turtle("square")
+#             new_car.shapesize(stretch_wid=1, stretch_len=2)
+#             new_car.penup()
+#             new_car.color(random.choice(COLORS))
+#             random_y = random.randint(-250, 250)
+#             new_car.goto(300, random_y)
+#             self.all_cars.append(new_car)
+
+#     def move_cars(self):
+#         for car in self.all_cars:
+#             car.backward(self.car_speed)
+
+#     def level_up(self):
+#         self.car_speed += MOVE_INCREMENT
+
+## [scoreboard.py]
+
+# from turtle import Turtle
+
+# FONT = ("Courier", 24, "normal")
+
+
+# class Scoreboard(Turtle):
+
+#     def __init__(self):
+#         super().__init__()
+#         self.level = 1
+#         self.hideturtle()
+#         self.penup()
+#         self.goto(-280, 250)
+#         self.update_scoreboard()
+
+
+#     def update_scoreboard(self):
+#         self.clear()
+#         self.write(f"Level: {self.level}", align = "left", font = FONT)
+
+
+#     def increase_level(self):
+#         self.level += 1
+#         self.update_scoreboard()
+
+#     def game_over(self):
+#         self.goto(0, 0)
+#         self.write(f"GAME OVER", align = "center", font = FONT)
+
+# ---------End of Day 23------------
+## Day 24 - Intermediate -Files, Directories and Paths
+
