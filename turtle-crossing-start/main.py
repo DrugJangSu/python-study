11d66431339ba770ad7b9b830096aa34d98df55d
DrugJangSu <ljonghyun6088@gmail.com>
2025-12-19 13:35:27 +0900

    Update

--- turtle-crossing-start/main.py
+++ turtle-crossing-start/main.py
@@ -29,6 +29,10 @@ while game_is_on:
         if car.distance(player) < 20: # collision
             game_is_on = False
 
+    # Detect successful crossing
+    if player.is_at_finish_line():
+        player.go_to_start()
+
 
 screen.exitonclick()
 
