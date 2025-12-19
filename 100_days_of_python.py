1d0e5b0bd4b43fa53e71bee8c8e3949ad5bb0147
DrugJangSu <ljonghyun6088@gmail.com>
2025-12-19 16:22:00 +0900

    Update

--- 100_days_of_python.py
+++ 100_days_of_python.py
@@ -8124,3 +8124,38 @@
 # ---------End of Day 23------------
 ## Day 24 - Intermediate -Files, Directories and Paths
 
+# Improving the snake game
+
+## How to open, read, and write to files using the "with" Keyword
+
+# import os
+# BASE_DIR = os.path.dirname(__file__)
+# file_path = os.path.join(BASE_DIR, "my_file.txt")
+# file = open(file_path)
+# contents = file.read()
+# print(contents)
+# file.close()
+
+# import os
+# BASE_DIR = os.path.dirname(__file__)
+# file_path = os.path.join(BASE_DIR, "my_file.txt")
+# with open(file_path) as file:
+#     contents = file.read()
+#     print(contents)
+
+
+# import os
+# BASE_DIR = os.path.dirname(__file__)
+# file_path = os.path.join(BASE_DIR, "my_file.txt")
+# with open(file_path, mode = "w") as file: ## w for write - but it overwrites.
+#     file.write("New text.")
+## when you try to open a file that doesn't exist while in "write mode," it'll make a new file.
+
+
+# import os
+# BASE_DIR = os.path.dirname(__file__)
+# file_path = os.path.join(BASE_DIR, "my_file.txt")
+# with open(file_path, mode = "a") as file: # a for append
+#     file.write("\nNew text.")
+
+
