877a7eb5faa9f7df949d48044e8aec0d12173a3e
DrugJangSu <ljonghyun6088@gmail.com>
2025-12-20 13:15:55 +0900

    Update

--- snake_game/scoreboard.py
+++ snake_game/scoreboard.py
@@ -1,4 +1,5 @@
 from turtle import Turtle
+import os
 ALIGNMENT = "center"
 FONT = ("Arial", 24, "normal")
 
@@ -8,6 +9,16 @@ class Scoreboard(Turtle):
         super().__init__()
         self.score = 0
         self.high_score = 0
+
+        self.BASE_DIR = os.path.dirname(__file__)
+        self.data_path = os.path.join(self.BASE_DIR, "data.txt")
+        try:
+            with open(self.data_path) as data:
+                self.high_score = int(data.read())
+        except (FileNotFoundError, ValueError):
+            with open(self.data_path, "w") as data:
+                data.write("0")
+        
         self.color("white")
         self.penup()
         self.goto(0, 270)
@@ -20,7 +31,9 @@ class Scoreboard(Turtle):
 
     def reset(self):
         if self.score > self.high_score:
-             self.high_score = self.score
+            self.high_score = self.score
+            with open(self.data_path, "w") as data:
+                data.write(str(self.high_score))
         self.score = 0
         self.update_scoreboard()
 
