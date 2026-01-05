51b74e3491db750e35b8cf4a9a2ab9136f586e74
DrugJangSu <ljonghyun6088@gmail.com>
2026-01-05 16:29:25 +0900

    Update

--- day_25(squirrel-challenge)/main.py
+++ day_25(squirrel-challenge)/main.py
@@ -1,28 +1,23 @@
+
+# #Using just file methods
 # with open("weather_data.csv") as data_file:
 #     data = data_file.readlines()
 #     print(data)
 
+
+# #Using csv library
 # import csv
 
 # with open("weather_data.csv") as data_file:
 #     data = csv.reader(data_file)
 #     temperatures = []
-#     print(data)
 #     for row in data:
 #         if row[1] != "temp":
 #             temperatures.append(int(row[1]))
 #     print(temperatures)
 
-## Using the panda library module, it's much more convenient
 
-# import pandas
-
-# data = pandas.read_csv("weather_data.csv")
-# # print(data)
-# print(data["temp"])
-# print(data["day"])
-
-
+# # Using the pandas library
 # import pandas
 
 # data = pandas.read_csv("weather_data.csv")
@@ -33,52 +28,49 @@
 # print(data_dict)
 
 # temp_list = data["temp"].to_list()
-# print(temp_list)
 # print(len(temp_list))
 
-# # <way to calculate average #1>
-# average =sum(temp_list) / len(temp_list)
-# print(average)
-
-# # <way to calculate average #2>
 # print(data["temp"].mean())
-
-# # <way to find max value>
 # print(data["temp"].max())
 
-# # Get Data in Columns
+# #Get Data in Columns
 # print(data["condition"])
-# print(data.condition) # another way to get a column
+# print(data.condition)
 
-## Be aware that if your column name has a Capital letter or space, you must use the same format.
-
-# Get Data in Rows
-# print(data[data.day == "Monday"])
-
-## Get row where temp is max
-# data["temp"].max()
-# data.temp.max()
-# data.temp == data.temp.max()
-
+# # Get Data in Row
 # print(data[data.day == "Monday"])
 # print(data[data.temp == data.temp.max()])
 
+# # Get Row data value
 # monday = data[data.day == "Monday"]
-# print(monday.condition)
-
-## get monday's temp in Fahrenheit
-
-# monday = data[data.day == "Monday"]
-# monday_temp = monday.temp[0]
+# monday_temp = int(monday.temp)
 # monday_temp_F = monday_temp * 9/5 + 32
 # print(monday_temp_F)
 
-# Create a dataframe from scratch
+# # Create a dataframe from scratch
 # data_dict = {
 #     "students": ["Amy", "James", "Angela"],
 #     "scores": [76, 56, 65]
 # }
 # data = pandas.DataFrame(data_dict)
 # data.to_csv("new_data.csv")
-# print(data)
-## Running the above code will create a new_data.csv file in the same directory.
+
+
+#Central Park Squirrel Data Analysis
+import pandas
+
+data = pandas.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
+grey_squirrels_count = len(data[data["Primary Fur Color"] == "Gray"])
+red_squirrels_count = len(data[data["Primary Fur Color"] == "Cinnamon"])
+black_squirrels_count = len(data[data["Primary Fur Color"] == "Black"])
+print(grey_squirrels_count)
+print(red_squirrels_count)
+print(black_squirrels_count)
+
+data_dict = {
+    "Fur Color": ["Gray", "Cinnamon", "Black"],
+    "Count": [grey_squirrels_count, red_squirrels_count, black_squirrels_count]
+}
+
+df = pandas.DataFrame(data_dict)
+df.to_csv("squirrel_count.csv")
