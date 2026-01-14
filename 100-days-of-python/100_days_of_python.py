c8e15302d688e17ee8112e9036b029452e15c107
DrugJangSu <ljonghyun6088@gmail.com>
2026-01-14 17:46:57 +0900

    Update

--- 100-days-of-python/100_days_of_python.py
+++ 100-days-of-python/100_days_of_python.py
@@ -10088,3 +10088,205 @@ from tkinter import messagebox
 ## -----End of Day 30------------------------------------------------------------------------
 ## Day 31 - Intermediate - Flash Card App Capstone Project
 
+## Step 1 - Create the User Interface (UI) with Tkinter---------------
+# from tkinter import *
+
+# BACKGROUND_COLOR = "#B1DDC6"
+
+# window = Tk()
+# window.title("Flashy")
+# window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+
+# canvas = Canvas(width=800, height=526)
+# card_front_img = PhotoImage(file="images/card_front.png")
+# canvas.create_image(400, 263, image=card_front_img)
+# canvas.create_text(400, 150, text="Title", fill="Black", font=("Arial", 40, "italic"))
+# canvas.create_text(400, 263, text="word", fill="Black", font=("Arial", 60, "bold"))
+
+# canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
+# canvas.grid(row=0, column=0, columnspan=2)
+
+# cross_image = PhotoImage(file="images/wrong.png")
+# unknown_button = Button(image=cross_image, highlightthickness=0)
+# unknown_button.grid(row=1, column=0)
+
+# check_image = PhotoImage(file="images/right.png")
+# known_button = Button(image=check_image, highlightthickness=0)
+# known_button.grid(row=1, column=1)
+
+
+# window.mainloop()
+
+## Step 2 - Create New Flash Cards---------------
+# from tkinter import *
+# import pandas
+# import random
+
+# BACKGROUND_COLOR = "#B1DDC6"
+
+# data = pandas.read_csv("data/french_words.csv")
+# print(data)
+# to_learn = data.to_dict(orient="records")
+
+
+# def next_card():
+#     current_card = random.choice(to_learn)
+#     canvas.itemconfig(card_title, text="French")
+#     canvas.itemconfig(card_word, text=current_card["French"])
+
+# window = Tk()
+# window.title("Flashy")
+# window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+
+# canvas = Canvas(width=800, height=526)
+# card_front_img = PhotoImage(file="images/card_front.png")
+# canvas.create_image(400, 263, image=card_front_img)
+# card_title = canvas.create_text(400, 150, text="", fill="Black", font=("Arial", 40, "italic"))
+# card_word = canvas.create_text(400, 263, text="", fill="Black", font=("Arial", 60, "bold"))
+
+# canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
+# canvas.grid(row=0, column=0, columnspan=2)
+
+# cross_image = PhotoImage(file="images/wrong.png")
+# unknown_button = Button(image=cross_image, highlightthickness=0, command=next_card)
+# unknown_button.grid(row=1, column=0)
+
+# check_image = PhotoImage(file="images/right.png")
+# known_button = Button(image=check_image, highlightthickness=0, command=next_card)
+# known_button.grid(row=1, column=1)
+
+# next_card()
+
+
+# window.mainloop()
+
+
+## Step 3 - Flip the Cards---------------
+# from tkinter import *
+# import pandas
+# import random
+
+# BACKGROUND_COLOR = "#B1DDC6"
+
+# data = pandas.read_csv("data/french_words.csv")
+# print(data)
+# to_learn = data.to_dict(orient="records")
+# current_card = {}
+
+# def next_card():
+#     global current_card, flip_timer
+#     window.after_cancel(flip_timer)
+#     current_card = random.choice(to_learn)
+#     canvas.itemconfig(card_title, text="French", fill="black")
+#     canvas.itemconfig(card_word, text=current_card["French"], fill="black")
+#     canvas.itemconfig(card_background, image=card_front_img)
+#     flip_timer = window.after(3000, func=flip_card)
+
+
+# def flip_card():
+#     canvas.itemconfig(card_title, text="English", fill="white")
+#     canvas.itemconfig(card_word, text=current_card["English"], fill="white")
+#     canvas.itemconfig(card_background, image=card_back_img)
+
+# window = Tk()
+# window.title("Flashy")
+# window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+
+# flip_timer = window.after(3000, func=flip_card)
+
+# canvas = Canvas(width=800, height=526)
+# card_front_img = PhotoImage(file="images/card_front.png")
+# card_back_img = PhotoImage(file="images/card_back.png")
+# card_background = canvas.create_image(400, 263, image=card_front_img)
+# card_title = canvas.create_text(400, 150, text="", fill="Black", font=("Arial", 40, "italic"))
+# card_word = canvas.create_text(400, 263, text="", fill="Black", font=("Arial", 60, "bold"))
+
+# canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
+# canvas.grid(row=0, column=0, columnspan=2)
+
+# cross_image = PhotoImage(file="images/wrong.png")
+# unknown_button = Button(image=cross_image, highlightthickness=0, command=next_card)
+# unknown_button.grid(row=1, column=0)
+
+# check_image = PhotoImage(file="images/right.png")
+# known_button = Button(image=check_image, highlightthickness=0, command=next_card)
+# known_button.grid(row=1, column=1)
+
+# next_card()
+
+
+# window.mainloop()
+
+
+## Step 4 - Save Your Progress---------------
+
+# from tkinter import *
+# import pandas
+# import random
+
+# BACKGROUND_COLOR = "#B1DDC6"
+# current_card = {}
+# to_learn = {}
+
+# try:
+#     data = pandas.read_csv("data/words_to_learn.csv")
+# except FileNotFoundError:
+#     original_data = pandas.read_csv("data/french_words.csv")
+#     to_learn = original_data.to_dict(orient="records")
+# else:
+#     to_learn = data.to_dict(orient="records")
+
+# def next_card():
+#     global current_card, flip_timer
+#     window.after_cancel(flip_timer)
+#     current_card = random.choice(to_learn)
+#     canvas.itemconfig(card_title, text="French", fill="black")
+#     canvas.itemconfig(card_word, text=current_card["French"], fill="black")
+#     canvas.itemconfig(card_background, image=card_front_img)
+#     flip_timer = window.after(3000, func=flip_card)
+
+# def flip_card():
+#     canvas.itemconfig(card_title, text="English", fill="white")
+#     canvas.itemconfig(card_word, text=current_card["English"], fill="white")
+#     canvas.itemconfig(card_background, image=card_back_img)
+
+# def is_known():
+#     to_learn.remove(current_card)
+#     print(len(to_learn))
+#     data = pandas.DataFrame(to_learn)
+#     data.to_csv("data/words_to_learn.csv", index=False)
+
+#     next_card()
+
+# window = Tk()
+# window.title("Flashy")
+# window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+
+# flip_timer = window.after(3000, func=flip_card)
+
+# canvas = Canvas(width=800, height=526)
+# card_front_img = PhotoImage(file="images/card_front.png")
+# card_back_img = PhotoImage(file="images/card_back.png")
+# card_background = canvas.create_image(400, 263, image=card_front_img)
+# card_title = canvas.create_text(400, 150, text="", fill="Black", font=("Arial", 40, "italic"))
+# card_word = canvas.create_text(400, 263, text="", fill="Black", font=("Arial", 60, "bold"))
+
+# canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
+# canvas.grid(row=0, column=0, columnspan=2)
+
+# cross_image = PhotoImage(file="images/wrong.png")
+# unknown_button = Button(image=cross_image, highlightthickness=0, command=next_card)
+# unknown_button.grid(row=1, column=0)
+
+# check_image = PhotoImage(file="images/right.png")
+# known_button = Button(image=check_image, highlightthickness=0, command=is_known)
+# known_button.grid(row=1, column=1)
+
+# next_card()
+
+
+# window.mainloop()
+
+## -----End of Day 31-------------------------------------------------------------------------
+## Day 32 - Intermediate+ Send email (smtplib) & Manage Dates (datetime)
+
