8a360c0c5e016a288b6a14690517a9b226161a5b
DrugJangSu <ljonghyun6088@gmail.com>
2026-01-16 14:07:23 +0900

    Update

--- 100-days-of-python/100_days_of_python.py
+++ 100-days-of-python/100_days_of_python.py
@@ -10468,6 +10468,133 @@ from tkinter import messagebox
 
 
 # window.mainloop()
-
+# ----------------------------------------------------------------------
 ## Understand API Parameters: Match Sunset Times with the Current Time---------
 
+# Sunset and Sunrise free API
+
+# lat(float) : Latitude in decimal degrees
+# lng(float) : Longitude in decimal degrees
+import requests
+from datetime import datetime
+
+MY_LAT = 37.566536
+MY_LONG = 126.977966
+# (It's the coordinates for Seoul, lol)
+
+parameters = {
+    "lat": MY_LAT,
+    "lng": MY_LONG,
+    "formatted" : 0
+}
+
+response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+response.raise_for_status()
+data = response.json()
+# print(data)
+
+# https://api.sunrise-sunset.org/json?lat=37.566536&lng=126.977966 -> This will also work.
+sunrise = data["results"]["sunrise"].split("T")[1].split(":")[0]
+sunset = data["results"]["sunset"].split("T")[1].split(":")[0]
+
+print(sunrise)
+print(sunset)
+
+time_now = datetime.now()
+
+print(time_now.hour)
+
+
+## ISS OVERHEAD NOTIFIER PROJECT CHALLENGE
+
+import requests
+from datetime import datetime
+
+MY_LAT = 37.566536 # Your latitude
+MY_LONG = 126.977966 # Your longitude
+
+response = requests.get(url="http://api.open-notify.org/iss-now.json")
+response.raise_for_status()
+data = response.json()
+
+iss_latitude = float(data["iss_position"]["latitude"])
+iss_longitude = float(data["iss_position"]["longitude"])
+
+#Your position is within +5 or -5 degrees of the ISS position.
+
+parameters = {
+    "lat": MY_LAT,
+    "lng": MY_LONG,
+    "formatted": 0,
+}
+
+response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+response.raise_for_status()
+data = response.json()
+sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+
+time_now = datetime.now()
+
+#If the ISS is close to my current position
+# and it is currently dark
+# Then send me an email to tell me to look up.
+# BONUS: run the code every 60 seconds.
+
+# <Solution>---------------------------
+# import requests
+# from datetime import datetime
+# import smtplib
+# import time
+
+# MY_EMAIL = "00000000000@gmail.com"
+# MY_PASSWORD = "0000000000000000"
+# MY_LAT = 37.566536
+# MY_LONG = 126.977966
+
+# def is_iss_overhead():
+#     response = requests.get(url="http://api.open-notify.org/iss-now.json")
+#     response.raise_for_status()
+#     data = response.json()
+
+#     iss_latitude = float(data["iss_position"]["latitude"])
+#     iss_longitude = float(data["iss_position"]["longitude"])
+
+#     #Your position is within +5 or -5 degrees of the ISS position.
+#     if MY_LAT-5 <= iss_latitude <= MY_LAT+5 and MY_LONG-5 <= iss_longitude <= MY_LONG+5:
+#         return True
+
+# def is_night():
+#     parameters = {
+#         "lat": MY_LAT,
+#         "lng": MY_LONG,
+#         "formatted": 0,
+#     }
+
+#     response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+#     response.raise_for_status()
+#     data = response.json()
+#     sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+#     sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+
+#     time_now = datetime.now().hour
+
+#     if time_now >= sunset or time_now <= sunrise:
+#         return True
+
+
+# while True:
+#     time.sleep(60)
+#     if is_iss_overhead() and is_night():
+#         connection = smtplib.SMTP("smtp.gmail.com")
+#         connection.starttls()
+#         connection.login(MY_EMAIL, MY_PASSWORD)
+#         connection.sendmail(
+#             from_addr=MY_EMAIL,
+#             to_addrs=MY_EMAIL,
+#             msg="Subject:Look Up👆\n\nThe ISS is above you in the sky."
+#         )
+
+
+## End of Day 33 Project ------------------------------------------
+## Day 34 - Intermediate+ API Practice - Creating a GUI Quiz App
