79504075454d9d88e00c91003bc4c2eda187f4da
DrugJangSu <ljonghyun6088@gmail.com>
2026-01-23 16:30:20 +0900

    Update

--- 100-days-of-python/100_days_of_python.py
+++ 100-days-of-python/100_days_of_python.py
@@ -11912,34 +11912,34 @@ from tkinter import messagebox
 # dirty_dozen = [fruits, vegetables]
 # print(dirty_dozen)
 
-import random
+# import random
 
-rock = """
-    _______
----'   ____)
-      (_____)
-      (_____)
-      (____)
----.__(___)
-"""
+# rock = """
+#     _______
+# ---'   ____)
+#       (_____)
+#       (_____)
+#       (____)
+# ---.__(___)
+# """
 
-paper ="""
-     _______
----'    ____)____
-           ______)
-          _______)
-         _______)
----.__________)
-"""
+# paper ="""
+#      _______
+# ---'    ____)____
+#            ______)
+#           _______)
+#          _______)
+# ---.__________)
+# """
 
-scissors ="""
-    _______
----'   ____)____
-          ______)
-       __________)
-      (____)
----.__(___)
-"""
+# scissors ="""
+#     _______
+# ---'   ____)____
+#           ______)
+#        __________)
+#       (____)
+# ---.__(___)
+# """
 
 # <My solution>
 # print("Welcome to Rock, Paper, Scissors!")
@@ -12088,8 +12088,203 @@ scissors ="""
 
 ## Day 40 - Intermediate+ Capstone Part 2 : Flight Club --------------------------------------------------------------------------------------------
 
-# <Quite a lot of mismatch with the site and the project, gonna return later>
+# <vice versa>
 
 
 # --------------------------------------------------------------------------------------------
 ## Day 41- Web Foundation - Introduction to HTML
+
+## How Do Websites Work?
+# HTML, CSS, JS
+# HTML : Responsible for the structure of the website
+# CSS : Responsible for the styles of the website
+# JavaScript : Responsible for the behaviors and the actions of the website
+
+## HTML(HyperText Markup Language)
+# Hypertext is a text which contains links to other texts.
+# Markup Language is literally a "quotation" as to tell what to do for each.
+
+
+## The Heading Element
+# <h1>Hello World</h1>
+# In this case, Hello World is the content.
+# <> - we call these angle brackets.
+# <h1> is the opening tag.
+# </h1> is the closing tag.
+
+# <h1>Heading 1</h1>
+# <h2>Heading 2</h2>
+# <h3>Heading 3</h3>
+# <h4>Heading 4/h4>
+# <h5>Heading 5</h5>
+# <h6>Heading 6</h6>
+## h7 does not exist.
+
+# <h1>Book</h1>
+#     <h2>Chapter 1</h2>
+#         <h3>Section 1</h3>
+#         <h3>Section 2</h3>
+#     <h2>Chapter 2</h2>
+#         <h3>Section 1</h3>
+#             <h4>Diagram 1</h4>
+#     <h2>Chapter 3</h2>
+#         <h3>Section 1</h3>
+#         <h3>Section 2</h3>
+## It's not a good idea to have more than one h1. If you need more than one, mostly go for h2 or h3 etc.
+## Don't skip a level. Don't go like h1 -> h3, h2 -> h5 etc.
+## Always go from h1 -> h2 -> h3. It won't cause an error, but it's mostly conventional rules
+
+# The Paragtaph Element
+# <p>This is a paragraph.</p>
+# <p>
+#   First paragraph. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+#   do eiusmod tempor incididunt ut labore et dolore magna aliqua. Arcu cursus
+#   vitae congue mauris. In nisl nisi scelerisque eu ultrices vitae auctor eu
+#   augue. Nisi est sit amet facilisis magna. Diam sit amet nisl suscipit
+#   adipiscing bibendum est ultricies integer. Quis ipsum suspendisse ultrices
+#   gravida dictum fusce ut. Euismod elementum nisi quis eleifend. Habitant morbi
+#   tristique senectus et. Amet nisl suscipit adipiscing bibendum est ultricies
+#   integer. Viverra orci sagittis eu volutpat odio facilisis mauris sit. Nisi
+#   quis eleifend quam adipiscing. Neque convallis a cras semper auctor neque
+#   vitae. Magna fermentum iaculis eu non. Vivamus arcu felis bibendum ut
+#   tristique et. Justo nec ultrices dui sapien eget mi. In vitae turpis massa sed
+#   elementum tempus. Eu facilisis sed odio morbi quis commodo. Sagittis aliquam
+#   malesuada bibendum arcu vitae elementum curabitur vitae.
+# </p>
+
+# <p>
+#   Second paragraph. Suscipit adipiscing bibendum est ultricies. Tortor aliquam
+#   nulla facilisi cras fermentum. Eget aliquet nibh praesent tristique magna. In
+#   hac habitasse platea dictumst vestibulum. Ornare quam viverra orci sagittis
+#   eu. Sit amet est placerat in. Proin fermentum leo vel orci porta non pulvinar
+#   neque laoreet. Turpis in eu mi bibendum neque egestas congue. Enim eu turpis
+#   egestas pretium aenean pharetra magna ac placerat. Ultrices sagittis orci a
+#   scelerisque purus semper eget duis at. Egestas egestas fringilla phasellus
+#   faucibus scelerisque eleifend donec pretium. Condimentum lacinia quis vel eros
+#   donec ac odio.
+# </p>
+
+# <p>
+#   Third paragraph. Nisl purus in mollis nunc sed id semper risus. Ipsum a arcu
+#   cursus vitae congue mauris rhoncus aenean. Ridiculus mus mauris vitae
+#   ultricies leo integer malesuada nunc. In tellus integer feugiat scelerisque.
+#   Lectus mauris ultrices eros in cursus turpis massa. Sollicitudin ac orci
+#   phasellus egestas. Massa massa ultricies mi quis hendrerit dolor. Quam
+#   elementum pulvinar etiam non quam lacus suspendisse faucibus interdum. Iaculis
+#   nunc sed augue lacus viverra. Id ornare arcu odio ut sem nulla pharetra. Amet
+#   luctus venenatis lectus magna fringilla urna porttitor. Eu nisl nunc mi ipsum
+#   faucibus vitae aliquet nec ullamcorper. Nunc mattis enim ut tellus elementum
+#   sagittis. Mauris augue neque gravida in fermentum et sollicitudin.
+#   Pellentesque habitant morbi tristique senectus. Tristique senectus et netus
+#   et. Turpis egestas sed tempus urna et pharetra pharetra. Feugiat vivamus at
+#   augue eget arcu dictum varius duis at. Lacus sed viverra tellus in hac
+#   habitasse platea dictumst vestibulum. Nisl condimentum id venenatis a
+#   condimentum vitae sapien.
+# </p>
+
+
+## Self Closing Tags
+
+## Horizontal Rule Element
+# <hr /> -> This is a void element, in which you're forbidden from putting anything inside the tag.
+
+# <p>This is a paragraph.</p>
+# <hr /> 
+# <p>This is a paragraph.</p>
+
+# In this case, this is a way to distnguish between the paragraph(as a horizontal line)
+
+## Break Element
+# <br />
+# A way to separate lines in the same paragraph.
+# <p>
+# line<br />
+# line<br />
+# </p>
+# It's mostly used in poems
+
+# <h1>William Blake</h1>
+
+# <p>
+# 17 south molton street<br />
+# London<br />
+# W1K 5QT<br />
+# UK<br />
+# <hr />
+# </p>
+# <p>
+#   William Blake (28 November 1757 -- 12 August 1827) was an English poet,
+#   painter, and printmaker. Largely unrecognised during his life, Blake is now
+#   considered a seminal figure in the history of the poetry and visual art of the
+#   Romantic Age. What he called his "prophetic works" were said by 20th-century
+#   critic Northrop Frye to form "what is in proportion to its merits the least
+#   read body of poetry in the English language".[2] His visual artistry led
+#   21st-century critic Jonathan Jones to proclaim him "far and away the greatest
+#   artist Britain has ever produced".[3] In 2002, Blake was placed at number 38
+#   in the BBC's poll of the 100 Greatest Britons.[4] While he lived in London his
+#   entire life, except for three years spent in Felpham,[5] he produced a diverse
+#   and symbolically rich collection of works, which embraced the imagination as
+#   "the body of God"[6] or "human existence itself".[7]
+# </p>
+# <p>
+#   Although Blake was considered mad by contemporaries for his idiosyncratic
+#   views, he is held in high regard by later critics for his expressiveness and
+#   creativity, and for the philosophical and mystical undercurrents within his
+#   work. His paintings and poetry have been characterised as part of the Romantic
+#   movement and as "Pre-Romantic".[8] In fact, he has been said to be "a key
+#   early proponent of both Romanticism and Nationalism".[9] A committed Christian
+#   who was hostile to the Church of England (indeed, to almost all forms of
+#   organised religion), Blake was influenced by the ideals and ambitions of the
+#   French and American revolutions.[10][11] Though later he rejected many of
+#   these political beliefs, he maintained an amiable relationship with the
+#   political activist Thomas Paine; he was also influenced by thinkers such as
+#   Emanuel Swedenborg.[12] Despite these known influences, the singularity of
+#   Blake's work makes him difficult to classify. The 19th-century scholar William
+#   Michael Rossetti characterised him as a "glorious luminary",[13] and "a man
+#   not forestalled by predecessors, nor to be classed with contemporaries, nor to
+#   be replaced by known or readily surmisable successors".[14]
+# </p>
+
+
+# Nowadays HTML5 or the latest version will understand without the void element(<hr>, <br> etc) however it's recommended to use the void element to fully understand the html.
+## Day 41 Project : Movie Ranking
+# <my solution>
+# <h1>The Best Movies According to Angela</h1>
+# <h2>My top 3 movies of all-time.</h2>
+# <hr />
+# <h3>Spirited Away</h3>
+# <p>This is my favourite anime. I love the beautiful images.</p>
+# <h3>Ex Machina</h3>
+# <p>Really cool sci-fi movie.</p>
+# <h3>Drive</h3>
+# <p>Super beautiful film. Really artistic.</p>
+# <h1>The Best movies According to Hyun</h1>
+# <h2>My top 5 movies of all-time.</h2>
+# <hr />
+# <h3>Mad Max: Fury Road</h3>
+# <p>What a day. WHAT A LOVELY DAY!</p>
+# <h3>John Wick series</h3>
+# <p>
+#   The absolute gun pornography. Every guy imagined to be John Wick once in their
+#   lifetime.
+# </p>
+# <h3>Spring Of Seoul</h3>
+# <p>If you fail it's called treason, if you win it's called a revolution.</p>
+# <h3>Boochi The Rock</h3>
+# <p>Probably the only anime I ever watched and liked. SEASON 2 PLS</p>
+# <h3>No Country for Old Men</h3>
+# <p>One of the masterpieces in the 2000s, change my mind.</p>
+
+# <angela's solution>
+# <h1>The Best Movies According to Angela</h1>
+# <h2>My top 3 movies of all-time.</h2>
+# <hr />
+# <h3>Spirited Away</h3>
+# <p>This is my favourite anime. I love the beautiful images.</p>
+# <h3>Ex Machina</h3>
+# <p>Really cool sci-fi movie.</p>
+# <h3>Drive</h3>
+# <p>Super beautiful film. Really artistic.</p>
+## End of Day 41 -----------------------------------------------------------------------------------------------------------------------------------
+
+## Day 42 - Web Foundation - Intermediate HTML
