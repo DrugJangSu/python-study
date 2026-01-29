f67fb88b602b0e946d800d97c8620cc0d1f0a1f1
DrugJangSu <ljonghyun6088@gmail.com>
2026-01-29 14:41:51 +0900

    Update

--- 100-days-of-python/100_days_of_python.py
+++ 100-days-of-python/100_days_of_python.py
@@ -13387,3 +13387,29 @@ from tkinter import messagebox
 # ---End of day 45-----------------------------------------------------------------------------------------------------------------------------------
 
 ## Day 46 - Intermediate+ Create a Spotify Playlist using the Musical Time Machine
+## Step 1 : Scraing the Billboard Hot 100---------------------------
+# import requests
+# from bs4 import BeautifulSoup
+
+# date = input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD: ")
+# year = date.split("-")[0]
+
+# ## Due to the Billboard chart is now asking for a pro subscription, gonna change to the wikipedia
+ 
+# url = f"https://en.wikipedia.org/wiki/Billboard_Year-End_Hot_100_singles_of_{year}"
+# header = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:131.0) Gecko/20100101 Firefox/131.0"}
+
+# response = requests.get(url=url, headers=header)
+# soup = BeautifulSoup(response.text, 'html.parser')
+
+# song_names_spans = soup.select("table.wikitable tbody tr td:nth-of-type(2)")
+# song_names = [song.getText().strip().strip('"') for song in song_names_spans]
+
+# print(song_names[:10])
+
+### Return later for the Spotify part-----------------------------------------------------------------------------------------------------------------------------------
+
+
+
+## Day 47 - Intermediate+ Create an Automated Amazon Price Tracker
+
